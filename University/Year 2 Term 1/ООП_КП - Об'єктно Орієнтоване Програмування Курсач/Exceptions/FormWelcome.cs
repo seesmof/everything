0a467595905fc8af899d9ac9c42b02fc1b24a079
Oleh Onyshchenko <seesmwork@gmail.com>
2023-10-24 10:23:20 +0300

    feat: add exceptions to pass to our exceptions guy for him to commit

---
+++ University/Year 2 Term 1/ООП_КП - Об'єктно Орієнтоване Програмування Курсач/Exceptions/FormWelcome.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace code.Forms
+{
+    public partial class FormWelcome : Form
+    {
+        private List<string> tips = new List<string> {
+            "У секції Профіль можна переглянути всі публікації, які ви створили",
+            "У секції Профіль можна переглянути всі нерухомості, які ви придбали",
+            "У секції Профіль можна змінити персональні дані",
+            "У секції Ринок можна додавати свої публікації",
+            "У секції Ринок можна купувати нерухомість",
+            "У секції Ринок можна сортувати та фільтрувати публікації",
+            "У секції Ринок можна шукати бажану нерухомість",
+            "У секції Ринок можна шукати бажану нерухомість",
+            "У секції Допомога можна отримати загальну інформацію про ринок",
+            "У секції Про розробників можна дізнатися більше про розробників",
+        };
+
+        private Random rnd = new Random();
+
+        public FormWelcome()
+        {
+            InitializeComponent();
+            this.Text = string.Empty;
+            this.ControlBox = false;
+            DisplayRandomTip();
+        }
+
+        private void DisplayRandomTip()
+        {
+            try
+            {
+                if (tips.Count == 0)
+                {
+                    throw new GeneralException("Підказок не знайдено");
+                }
+
+                int randomTipIndex = rnd.Next(tips.Count);
+                string randomTip = tips[randomTipIndex];
+
+                labelRandomTip.Text = randomTip;
+            }
+            catch (GeneralException ex)
+            {
+                ex.Display();
+            }
+        }
+    }
+}
---
+++ University/Year 2 Term 1/ООП_КП - Об'єктно Орієнтоване Програмування Курсач/Exceptions/GeneralException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace code
+{
+    public class GeneralException : Exception
+    {
+        public GeneralException()
+        {
+        }
+
+        public GeneralException(string message) : base(message) { }
+
+        public GeneralException(string message, Exception inner) : base(message, inner) { }
+
+        public void Display()
+        {
+            MessageBox.Show(this.Message, "Йой... Шось пішло не так", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
