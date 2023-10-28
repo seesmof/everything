05ae4f1e01388304f849f52a2e08ba2dfdc34ac2
Oleh Onyshchenko <seesmwork@gmail.com>
2023-10-28 17:04:31 +0300

    feat: OPI lab 2 task a done

---
+++ University/Year 2 Term 1/OPI/tasks/lb3/dev/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+            string compressed = Compress(input);
+            Console.WriteLine($"Compressed: {compressed}");
+            string decompressed = Decompress(compressed);
+            Console.WriteLine($"Decompressed: {decompressed}");
+        }
+
+        private static string Compress(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] == input[i - 1] && !Char.IsWhiteSpace(input[i]))
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > 1)
+                    {
+                        sb.Append(input[i - 1].ToString() + count);
+                    }
+                    else
+                    {
+                        sb.Append(input[i - 1]);
+                    }
+                    count = 1;
+                }
+            }
+            if (count > 1)
+            {
+                sb.Append(input[input.Length - 1].ToString() + count);
+            }
+            else
+            {
+                sb.Append(input[input.Length - 1]);
+            }
+            return sb.ToString();
+        }
+
+        private static string Decompress(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsDigit(input[i]))
+                {
+                    count = int.Parse(input[i].ToString());
+                    while (count > 1)
+                    {
+                        sb.Append(input[i - 1]);
+                        count--;
+                    }
+                }
+                else
+                {
+                    sb.Append(input[i]);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ University/Year 2 Term 1/OPI/tasks/lb3/task.md
@@ -0,0 +1,24 @@
+# Робота з рядками
+
+Навчитися працювати з рядками у Visual Studio C#.
+
+## Завдання
+
+Виконати наступні загальні завдання:
+
+- розробити архіватор: символи рядки, що повторюються, заміняти на послідовність - {СимволЧислоПовторювань}, наприклад: «fehhh eryaaa» повинна перетворитися на рядок наступного вигляду «feh3 erya3». Також реалізувати зворотну функцію програми;
+- розробити аналізатор: рядок, що вводиться, інтерпретується програмою, яка виконує потрібні дії, задані користувачем у рядку. Реалізувати прості арифметичні операції (/ \* - +). Наприклад, при введенні рядки «2 плюс 5» або «2 + 5», результатом виконання програми повинно бути - «7»
+
+Виконати наступне індивідуальні завдання:
+
+- Продемонструвати роботу таких функцій: Chars, Remove, Insert.
+
+---
+
+Task A: Develop an archiver. This involves creating a function that takes a string and replaces repeating characters with a sequence of the character followed by the number of repetitions. For example, "fehhh eryaaa" should be transformed into "feh3 erya3". You also need to implement a function that can reverse this process.
+
+Task B: Develop an analyzer. This involves creating a function that interprets a string as a command to perform a simple arithmetic operation. For example, if the input string is "2 plus 5" or "2 + 5", the function should return "7".
+
+Task C: Demonstrate the use of the Chars, Remove, and Insert functions. This task involves showing how these functions work, likely within the context of the string manipulation tasks above.
+
+As for your question about creating a C# console app, the task description does not explicitly state this. However, given that the tasks involve developing functions and demonstrating their use, and the context is working with strings in Visual Studio C#, it would be reasonable to create a C# console application to accomplish these tasks.
