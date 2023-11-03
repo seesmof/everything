8d508e919940f52efd8bc6aeab8548f4b0157b60
Oleh Onyshchenko <seesmwork@gmail.com>
2023-11-03 17:34:58 +0200

    feat: A DONE BITCH

--- University/Year 2 Term 1/OPI/tasks/lb4/prod/TaskA.cs
+++ University/Year 2 Term 1/OPI/tasks/lb4/prod/TaskA.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,37 @@ namespace dev
     {
         static void Main(string[] args)
         {
+            string loadedInput = LoadInput();
+            if (loadedInput != null)
+            {
+                Console.Write("Last input: ");
+                Console.WriteLine(loadedInput);
+            }
+
             Console.Write("Enter a string: ");
             string input = Console.ReadLine();
             string compressed = Compress(input);
             Console.WriteLine($"Compressed: {compressed}");
             string decompressed = Decompress(compressed);
             Console.WriteLine($"Decompressed: {decompressed}");
+
+            SaveInput(input);
+        }
+
+        private static string LoadInput()
+        {
+            string filename = "prevInput.txt";
+            if (File.Exists(filename))
+            {
+                return File.ReadAllLines(filename)?.Last();
+            }
+
+            return null;
+        }
+
+        private static void SaveInput(string input)
+        {
+            File.AppendAllText("prevInput.txt", input + Environment.NewLine);
         }
 
         private static string Compress(string input)
