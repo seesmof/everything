19f175d3285a7a1ad26290d10c713f0f75932e87
Oleh Onyshchenko <seesmwork@gmail.com>
2023-11-04 09:31:50 +0200

    feat: FAKCA FFMASTOJETSDIJKFSHFSDLJKFHFDSAJLFDSAKLJSADLKSFADJFASDL

---
+++ University/Year 2 Term 1/OPI/tasks/lb5/prod/TaskA.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace dev
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.btnMain = new System.Windows.Forms.Button();
+            this.lblMousePos = new System.Windows.Forms.Label();
+            this.lblBtnPos = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.trackBarSpeed = new System.Windows.Forms.TrackBar();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnMain
+            // 
+            this.btnMain.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnMain.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMain.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnMain.Location = new System.Drawing.Point(229, 175);
+            this.btnMain.Name = "btnMain";
+            this.btnMain.Size = new System.Drawing.Size(160, 40);
+            this.btnMain.TabIndex = 0;
+            this.btnMain.Text = "Hover over me";
+            this.btnMain.UseVisualStyleBackColor = false;
+            this.btnMain.Click += new System.EventHandler(this.btnMain_Click);
+            // 
+            // lblMousePos
+            // 
+            this.lblMousePos.AutoSize = true;
+            this.lblMousePos.Location = new System.Drawing.Point(12, 12);
+            this.lblMousePos.Name = "lblMousePos";
+            this.lblMousePos.Size = new System.Drawing.Size(141, 16);
+            this.lblMousePos.TabIndex = 1;
+            this.lblMousePos.Text = "Mouse Position Here...";
+            // 
+            // lblBtnPos
+            // 
+            this.lblBtnPos.AutoSize = true;
+            this.lblBtnPos.Location = new System.Drawing.Point(12, 37);
+            this.lblBtnPos.Name = "lblBtnPos";
+            this.lblBtnPos.Size = new System.Drawing.Size(137, 16);
+            this.lblBtnPos.TabIndex = 1;
+            this.lblBtnPos.Text = "Button Position Here...";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnReset.FlatAppearance.BorderSize = 0;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnReset.Location = new System.Drawing.Point(452, 12);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(120, 30);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset Button";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // trackBarSpeed
+            // 
+            this.trackBarSpeed.Location = new System.Drawing.Point(15, 91);
+            this.trackBarSpeed.Name = "trackBarSpeed";
+            this.trackBarSpeed.Size = new System.Drawing.Size(134, 45);
+            this.trackBarSpeed.TabIndex = 3;
+            this.trackBarSpeed.ValueChanged += new System.EventHandler(this.trackBarSpeed_ValueChanged);
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Location = new System.Drawing.Point(12, 62);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(90, 16);
+            this.lblSpeed.TabIndex = 1;
+            this.lblSpeed.Text = "Speed Here...";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.trackBarSpeed);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.lblBtnPos);
+            this.Controls.Add(this.lblMousePos);
+            this.Controls.Add(this.btnMain);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(600, 400);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "FormMain";
+            this.Text = "ОПІ Лабораторна 4 - Події миші";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormMain_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMain;
+        private System.Windows.Forms.Label lblMousePos;
+        private System.Windows.Forms.Label lblBtnPos;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TrackBar trackBarSpeed;
+        private System.Windows.Forms.Label lblSpeed;
+    }
+}
+
