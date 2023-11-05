ef2f73a1df6368588290b7e585be391d709be404
Oleh Onyshchenko <seesmwork@gmail.com>
2023-11-05 09:46:53 +0200

    refactor: edit class times scheduling to -5 minutes

--- Python Automations/tasks_scheduling.py
+++ Python Automations/tasks_scheduling.py
@@ -16,6 +16,13 @@ classFourTime = "13:25"
 classFiveTime = "14:55"
 classSixTime = "16:45"
 
+classOneTime_early = "08:25"
+classTwoTime_early = "10:00"
+classThreeTime_early = "11:50"
+classFourTime_early = "13:20"
+classFiveTime_early = "14:50"
+classSixTime_early = "16:40"
+
 try:
     clear_downloads_folder()
 except Exception:
@@ -37,46 +44,46 @@ else:
 
 if dayName == "Monday":
     speak_text(f"{ASD} Lecture at {classThreeTime}")
-    schedule.every().day.at(classThreeTime).do(ASD_lecture)
+    schedule.every().day.at(classThreeTime_early).do(ASD_lecture)
     if weekNominationStatus == "Чисельник":
         speak_text(f"{TY} Practice at {classFourTime}")
-        schedule.every().day.at(classFourTime).do(TY_practice)
+        schedule.every().day.at(classFourTime_early).do(TY_practice)
         speak_text(f"{SP} Lecture at {classFiveTime}")
-        schedule.every().day.at(classFiveTime).do(SP_lecture)
+        schedule.every().day.at(classFiveTime_early).do(SP_lecture)
     else:
         speak_text(f"{SP} Lab at {classFourTime}")
-        schedule.every().day.at(classFourTime).do(SP_practice)
+        schedule.every().day.at(classFourTime_early).do(SP_practice)
 elif dayName == "Tuesday":
     speak_text(f"{WEB} Lab at {classOneTime}")
-    schedule.every().day.at(classOneTime).do(WEB_practice)
+    schedule.every().day.at(classOneTime_early).do(WEB_practice)
     speak_text(f"{ASD} Lab at {classTwoTime}")
-    schedule.every().day.at(classTwoTime).do(ASD_practice)
+    schedule.every().day.at(classTwoTime_early).do(ASD_practice)
     speak_text(f"{OPI} Lab at {classFiveTime}")
-    schedule.every().day.at(classFiveTime).do(OPI_practice)
+    schedule.every().day.at(classFiveTime_early).do(OPI_practice)
     if weekNominationStatus == "Знаменник":
         speak_text(f"{OPI} Practice at {classFourTime}")
-        schedule.every().day.at(classFourTime).do(OPI_practice)
+        schedule.every().day.at(classFourTime_early).do(OPI_practice)
 elif dayName == "Wednesday":
     speak_text(f"{WEB} Lecture at {classTwoTime}")
-    schedule.every().day.at(classTwoTime).do(WEB_lecture)
+    schedule.every().day.at(classTwoTime_early).do(WEB_lecture)
     if weekNominationStatus == "Чисельник":
         speak_text(f"{TY} Lecture at {classOneTime}")
-        schedule.every().day.at(classOneTime).do(TY_lecture)
+        schedule.every().day.at(classOneTime_early).do(TY_lecture)
 elif dayName == "Thursday":
     if weekNominationStatus == "Чисельник":
         speak_text(f"{IY} Practice at {classFiveTime}")
-        schedule.every().day.at(classFiveTime).do(IY_practice)
+        schedule.every().day.at(classFiveTime_early).do(IY_practice)
     else:
         speak_text(f"{OPI} Lecture at {classTwoTime}")
-        schedule.every().day.at(classTwoTime).do(OPI_lecture)
+        schedule.every().day.at(classTwoTime_early).do(OPI_lecture)
         speak_text(f"{IY} Lecture at {classFiveTime}")
-        schedule.every().day.at(classFiveTime).do(IY_lecture)
+        schedule.every().day.at(classFiveTime_early).do(IY_lecture)
 elif dayName == "Friday":
     if weekNominationStatus == "Чисельник":
         speak_text(f"{VM} Lecture at {classTwoTime}")
-        schedule.every().day.at(classTwoTime).do(VM_lecture)
+        schedule.every().day.at(classTwoTime_early).do(VM_lecture)
         speak_text(f"{VM} Practice at {classThreeTime}")
-        schedule.every().day.at(classThreeTime).do(VM_practice)
+        schedule.every().day.at(classThreeTime_early).do(VM_practice)
 
 
 sunsetTime = get_sunset()
--- University/Year 2 Term 1/VM/tasks/lb2/task.md
+++ University/Year 2 Term 1/VM/tasks/lb2/task.md
@@ -1,14 +1,67 @@
-1. За ознаками збіжності дослідити збіжність рядів
-![](https://i.imgur.com/ECtKkNF.png)
-2. Дослідити збіжність заданих знакозмінних рядів
-![](https://i.imgur.com/iv3VBY0.png)
-3. Знайти інтервал збіжності та дослідити поведінку степеневого ряду на кінцях інтервалу збіжності
-![](https://i.imgur.com/xyxun7X.png)
-4. Розвинути функції у ряд по степенях x, використовуючи готові розвинення у ряд елементарних функцій
-![](https://i.imgur.com/Rtt1Wy1.png)
-5. Розвинути в ряд Фур'є задані функції
-![](https://i.imgur.com/OYm9KE1.png)
-6. Обчислити інтеграл з похибкою до 0.001
-![](https://i.imgur.com/syfPblk.png)
-7. Знайти перші чотири члени розвинення в ряд (до x^3) розв’язку диференційного рівняння за умовою у(0)=у'(0)=1
-![](https://i.imgur.com/q7SxpAc.png)
+#### 1
+
+За допомогою ознак збіжності дослідити збіжність рядів
+
+$$
+\displaystyle
+\sum_{n=1}^{\infty} \frac{n+3}{4n^3+2n+3}
+$$
+
+$$
+\displaystyle
+\sum_{n=1}^{\infty} (\ln 3+\frac{4}{n})^{-n}
+$$
+
+$$
+\displaystyle
+\sum_{n=1}^{\infty} \frac{n^3}{5^n}
+$$
+
+$$
+\displaystyle
+\sum_{n=1}^{\infty} \frac{3^{\frac{1}{n}}}{n^2}
+$$
+
+#### 2
+
+Дослідити збіжність заданих знакозмінних рядів
+
+$$
+\displaystyle
+\sum_{n=1}^{\infty} \frac{(-1)^n}{n+1}
+$$
+
+#### 5
+
+Розвинути в ряд Фур'є задані функції у вказаних інтервалах. Побудувати графіки самої функції та її перші чотири наближення розвинення у ряд Фур'є.
+
+$$
+\displaystyle
+f(x) = \begin{cases}
+-x, 0 \leq x < 1 \\
+x - 2, 1 \leq x < 2
+\end{cases}
+$$
+
+#### 6
+
+Розвинути в ряд Фур'є функції, які задано графічно
+
+```
+CONTEXT:
+This is supposed to be a graphical plot, but below I will describe it via text to the best of my ability. We will start moving from $x = -l$ and finish at $x = l$. The Y will vary from $-l/2$ to $l/2$, which I will indicate as we go.
+
+DESCRIPTION:
+We start at X=-l. We have a straight line that start at the X line on X=-l and goes straight down to Y=-l/2 from Y=0. If we were to try to describe it using numbers, imagine we are at X=-2 and Y=0. We have a line that goes from -2,0 to about -1,-1. Our Y=-1 is Y=-l/2. We are now at X=-1 and Y=-l/2. The line we had ends there and a new one begins at X=-1 and Y=0. It goes straight until X=0 and Y=l/2. Then it, or another line, doesnt matter, continues from the point of X=0, Y=l/2, straight down to X=1, Y=0. It ends there and a new one begins at X=1 and Y=-1 or Y=-l/2. It goes straight until X=2 or X=l and Y=0. At this point the plot ends.
+```
+
+![](https://i.imgur.com/llGwROP.png)
+
+#### 8
+
+Знайти перші п'ять членів розвинення в ряд розв'язку диференційного рівняння за умовою $y(0) = y'(0) = 1$
+
+$$
+\displaystyle
+y'' = 4y^2+2xe^{-y}
+$$
