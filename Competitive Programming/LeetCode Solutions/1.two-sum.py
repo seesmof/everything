3d92bc1f0b55794cc328653257fad22796b1d933
Oleh Onyshchenko <seesmwork@gmail.com>
2023-11-08 16:37:23 +0200

    feat: 1 yep

---
+++ Competitive Programming/LeetCode Solutions/1.two-sum.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: [int], target: int) -> [int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+# @lc code=end
