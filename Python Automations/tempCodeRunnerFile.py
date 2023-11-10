5293744481f1ac2e0da88b0ef41b9f73362559a0
Oleh Onyshchenko <seesmwork@gmail.com>
2023-11-10 20:01:26 +0200

    feat: FAIL

--- Python Automations/tempCodeRunnerFile.py
+++ Python Automations/tempCodeRunnerFile.py
@@ -1,3 +1,35 @@
-arr = [9, 9]
-res = solve(arr)
-print(res)
+class DynamicArray:
+    def __init__(self, capacity: int):
+        self.arr = [0] * capacity if capacity > 0 else []
+
+    def get(self, i: int) -> int:
+        return self.arr[i] if i <= len(self.arr) else -1
+
+    def set(self, i: int, n: int) -> None:
+        self.arr[i] = n if i <= len(self.arr) else -1
+
+    def pushback(self, n: int) -> None:
+        self.arr.append(n)
+
+    def popback(self) -> int:
+        self.arr.pop() if len(self.arr) > 0 else -1
+
+    def resize(self) -> None:
+        self.arr[:] = self.arr * 2
+
+    def getSize(self) -> int:
+        return len(self.arr)
+
+    def getCapacity(self) -> int:
+        pass
+
+
+arr = DynamicArray(5)
+print(arr.arr)
+arr.set(2, 5)
+print(arr.arr)
+arr.pushback(3)
+arr.pushback(9)
+arr.pushback(5)
+arr.pushback(11)
+print(arr.arr)
--- Python Automations/test.py
+++ Python Automations/test.py
@@ -1,4 +1,42 @@
-arr = [50, 100, 17, 98, 57, 37]
-lastIndex = arr.index(arr[-1])
-if lastIndex + 1 == len(arr):
-    print("YAY")
+class DynamicArray:
+    def __init__(self, capacity: int):
+        self.arr = [None] * capacity if capacity > 0 else [None]
+
+    def get(self, i: int) -> int:
+        return self.arr[i] if i <= len(self.arr) else -1
+
+    def set(self, i: int, n: int) -> None:
+        self.arr[i] = n if i <= len(self.arr) else -1
+
+    def pushback(self, n: int) -> None:
+        if self.arr and self.arr[-1] != None:
+            self.resize()
+        self.arr.append(n)
+
+    def popback(self) -> int:
+        self.arr.pop() if len(self.arr) > 0 else -1
+
+    def resize(self) -> None:
+        newOne = []
+        for _ in range(len(self.arr)):
+            newOne.append(None)
+        self.arr = self.arr + newOne
+
+    def getSize(self) -> int:
+        count = [el for el in self.arr if el != -1]
+        return len(count)
+
+    def getCapacity(self) -> int:
+        return len(self.arr)
+
+
+arr = DynamicArray(0)
+print(arr.arr)
+arr.pushback(5)
+print(arr.arr)
+arr.pushback(9)
+arr.pushback(3)
+arr.pushback(6)
+arr.pushback(12)
+arr.pushback(92)
+print(arr.arr)
