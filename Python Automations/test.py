e24ade70a7b1b28fbd6495fd0b96c31dac45b7d7
Oleh Onyshchenko <seesmwork@gmail.com>
2023-11-10 19:46:45 +0200

    feat: what?

--- Python Automations/test.py
+++ Python Automations/test.py
@@ -1,20 +1,4 @@
-def solve(arr):
-    requiredSize = len(set(arr))
-    uniquesCounter = 0
-    seens = {el: 0 for el in arr}
-
-    print(seens)
-    print(seens[arr[0]])
-
-    for i in range(len(arr)):
-        if seens[arr[i]] > 0:
-            arr.pop(i)
-        else:
-            seens[arr[i]] += 1
-
-    return uniquesCounter
-
-
-arr = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-res = solve(arr)
-print(arr)
+arr = [50, 100, 17, 98, 57, 37]
+lastIndex = arr.index(arr[-1])
+if lastIndex + 1 == len(arr):
+    print("YAY")
--- University/Year 2 Term 1/OPI/tasks/lb6/dev/FormMain.Designer.cs
+++ University/Year 2 Term 1/OPI/tasks/lb6/dev/FormMain.Designer.cs
@@ -28,22 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.gridLeaderboard = new System.Windows.Forms.DataGridView();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.btnSaveData = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridLeaderboard)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.Location = new System.Drawing.Point(16, 11);
+            this.lblOutput.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(511, 65);
+            this.lblOutput.TabIndex = 0;
+            this.lblOutput.Text = "Trainer text here...";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(19, 79);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(552, 22);
+            this.txtInput.TabIndex = 1;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(534, 11);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(38, 16);
+            this.lblTimer.TabIndex = 2;
+            this.lblTimer.Text = "00:00";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Orange;
+            this.btnReset.FlatAppearance.BorderSize = 0;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnReset.Location = new System.Drawing.Point(500, 107);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(71, 24);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Retry";
+            this.btnReset.UseVisualStyleBackColor = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnStart.FlatAppearance.BorderSize = 0;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStart.Location = new System.Drawing.Point(423, 107);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(71, 24);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            // 
+            // gridLeaderboard
+            // 
+            this.gridLeaderboard.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridLeaderboard.Location = new System.Drawing.Point(19, 146);
+            this.gridLeaderboard.Name = "gridLeaderboard";
+            this.gridLeaderboard.Size = new System.Drawing.Size(552, 171);
+            this.gridLeaderboard.TabIndex = 4;
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnLoadData.FlatAppearance.BorderSize = 0;
+            this.btnLoadData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoadData.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadData.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLoadData.Location = new System.Drawing.Point(468, 323);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(104, 24);
+            this.btnLoadData.TabIndex = 3;
+            this.btnLoadData.Text = "Load Data";
+            this.btnLoadData.UseVisualStyleBackColor = false;
+            // 
+            // btnSaveData
+            // 
+            this.btnSaveData.BackColor = System.Drawing.Color.Teal;
+            this.btnSaveData.FlatAppearance.BorderSize = 0;
+            this.btnSaveData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveData.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveData.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSaveData.Location = new System.Drawing.Point(358, 323);
+            this.btnSaveData.Name = "btnSaveData";
+            this.btnSaveData.Size = new System.Drawing.Size(104, 24);
+            this.btnSaveData.TabIndex = 3;
+            this.btnSaveData.Text = "Save Data";
+            this.btnSaveData.UseVisualStyleBackColor = false;
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.gridLeaderboard);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnSaveData);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblOutput);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximumSize = new System.Drawing.Size(600, 400);
             this.MinimumSize = new System.Drawing.Size(600, 400);
             this.Name = "FormMain";
             this.Text = "Робота з клавіатурою - ОПІ Лабораторна 6";
+            ((System.ComponentModel.ISupportInitialize)(this.gridLeaderboard)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataGridView gridLeaderboard;
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.Button btnSaveData;
     }
 }
 
