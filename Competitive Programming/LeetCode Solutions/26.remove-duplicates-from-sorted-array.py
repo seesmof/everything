8ff8508f676b8825e663f074466b1cb32a3fbb61
Oleh Onyshchenko <seesmwork@gmail.com>
2023-11-10 17:12:31 +0200

    feat: 26. for in place operations use [:]

--- Competitive Programming/LeetCode Solutions/26.remove-duplicates-from-sorted-array.py
+++ Competitive Programming/LeetCode Solutions/26.remove-duplicates-from-sorted-array.py
@@ -8,15 +8,8 @@
 # @lc code=start
 class Solution:
     def removeDuplicates(self, nums: [int]) -> int:
-        requiredSize = len(set(nums))
-        uniquesCounter = 0
-
-        #
-
-        # pop for the difference between array length and the needed size of set to remove unnecessary elements
-
-        # return the number of unique elements
-        return uniquesCounter
+        nums[:] = sorted(set(nums))
+        return len(nums)
 
 
 # @lc code=end
--- Python Automations/test.py
+++ Python Automations/test.py
@@ -2,7 +2,15 @@ def solve(arr):
     requiredSize = len(set(arr))
     uniquesCounter = 0
     seens = {el: 0 for el in arr}
+
     print(seens)
+    print(seens[arr[0]])
+
+    for i in range(len(arr)):
+        if seens[arr[i]] > 0:
+            arr.pop(i)
+        else:
+            seens[arr[i]] += 1
 
     return uniquesCounter
 
