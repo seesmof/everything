01c53b06f2c8637464268294a164dbd8d0220015
Oleh Onyshchenko <seesmwork@gmail.com>
2023-11-10 17:14:31 +0200

    feat: 27 yep

---
+++ Competitive Programming/LeetCode Solutions/27.remove-element.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=27 lang=python3
+#
+# [27] Remove Element
+#
+
+
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: [int], val: int) -> int:
+        nums[:] = [num for num in nums if num != val]
+        return len(nums)
+
+
+# @lc code=end
