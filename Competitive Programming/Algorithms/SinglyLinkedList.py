3cbbb9915e6e466dc838b7e9ba494b084f635808
Oleh Onyshchenko <seesmwork@gmail.com>
2023-11-12 10:54:43 +0200

    feat: minecraft start adding 1.20.1 mods and stuff. resource packs done

---
+++ Competitive Programming/Algorithms/SinglyLinkedList.py
@@ -0,0 +1,18 @@
+class LinkedList:
+    def __init__(self):
+        pass
+
+    def get(self, index: int) -> int:
+        pass
+
+    def insertHead(self, val: int) -> None:
+        pass
+
+    def insertTail(self, val: int) -> None:
+        pass
+
+    def remove(self, index: int) -> bool:
+        pass
+
+    def getValues(self) -> [int]:
+        pass
