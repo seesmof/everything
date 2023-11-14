561da689270c60d6fa12caa84f72bc2fadb9d198
Oleh Onyshchenko <seesmwork@gmail.com>
2023-11-14 09:27:48 +0200

    feat: whaaa?

--- University/Year 2 Term 1/OPI/tasks/lb6/dev/FormMain.cs
+++ University/Year 2 Term 1/OPI/tasks/lb6/dev/FormMain.cs
@@ -12,130 +12,176 @@ namespace dev
 {
     public partial class FormMain : Form
     {
-        private BestResults bestResults = new BestResults("bestresults.txt");
-        private char character;
-        private int time;
-        private int score;
-        private int maxTime;
-        private int minChars;
-        private int maxChars;
-        private int minWords;
-        private int maxWords;
-        private Random random;
+        // Global variables
+        char character; // The current character to be entered by the user
+        int time; // The remaining time for entering the character
+        int score; // The current score of the user
+        int maxTime; // The maximum time for entering the character based on the difficulty level
+        int minChars; // The minimum number of characters to be entered based on the difficulty level
+        int maxChars; // The maximum number of characters to be entered based on the difficulty level
+        int minWords; // The minimum number of words to be entered based on the difficulty level
+        int maxWords; // The maximum number of words to be entered based on the difficulty level
+        Random random; // A random number generator object
+        BestResults bestResults; // A best results object
 
 
         public FormMain()
         {
             InitializeComponent();
-
             tmrGame.Interval = 1000;
-            cmbDifficulty.SelectedIndex = 0;
 
+            // Load the best results from the file
+            bestResults = new BestResults("bestresults.txt");
             bestResults.Load();
+            // Set the default difficulty level to Easy
+            cmbDifficulty.SelectedIndex = 0;
+            // Reset the game variables and controls
             ResetGame();
         }
 
+        // A method to reset the game variables and controls
         private void ResetGame()
         {
+            // Set the character to null
             character = '\0';
+            // Set the time to 0
             time = 0;
+            // Set the score to 0
             score = 0;
+            // Set the maxTime, minChars, maxChars, minWords, and maxWords variables according to the difficulty level
             switch (cmbDifficulty.SelectedIndex)
             {
-                case 0:
+                case 0: // Easy
                     maxTime = 10;
                     minChars = 1;
                     maxChars = 1;
                     minWords = 1;
                     maxWords = 1;
                     break;
-                case 1:
-                    maxTime = 10;
-                    minChars = 3;
-                    maxChars = 5;
+                case 1: // Medium
+                    maxTime = 8;
+                    minChars = 1;
+                    maxChars = 2;
+                    minWords = 1;
+                    maxWords = 2;
+                    break;
+                case 2: // Hard
+                    maxTime = 6;
+                    minChars = 1;
+                    maxChars = 3;
                     minWords = 1;
                     maxWords = 3;
                     break;
-                case 2:
-                    maxTime = 8;
-                    minChars = 5;
-                    maxChars = 10;
+                case 3: // Expert
+                    maxTime = 4;
+                    minChars = 2;
+                    maxChars = 4;
+                    minWords = 2;
+                    maxWords = 4;
+                    break;
+                case 4: // Master
+                    maxTime = 2;
+                    minChars = 3;
+                    maxChars = 5;
                     minWords = 3;
                     maxWords = 5;
                     break;
-                case 3:
-                    maxTime = 6;
-                    minChars = 10;
-                    maxChars = 15;
-                    minWords = 5;
-                    maxWords = 10;
-                    break;
-                case 4:
-                    maxTime = 4;
-                    minChars = 15;
-                    maxChars = 20;
-                    minWords = 10;
-                    maxWords = 15;
-                    break;
             }
-            lblCharacter.Text = "Press Start to begin...";
+            // Set the character label to "Press Start to begin"
+            lblCharacter.Text = "Press Start to begin";
+            // Set the input text box to empty
             txtInput.Text = "";
+            // Set the timer label to "Time: 0"
             lblTimer.Text = "Time: 0";
+            // Set the score label to "Score: 0"
             lblScore.Text = "Score: 0";
+            // Enable the start button
             btnStart.Enabled = true;
+            // Disable the stop button
             btnStop.Enabled = false;
+            // Disable the input text box
             txtInput.Enabled = false;
+            // Enable the difficulty combo box
             cmbDifficulty.Enabled = true;
         }
 
+        // A method to generate a new character to be entered by the user
         private void GenerateCharacter()
         {
+            // Get a random number of characters between minChars and maxChars
             int chars = random.Next(minChars, maxChars + 1);
+            // Get a random number of words between minWords and maxWords
             int words = random.Next(minWords, maxWords + 1);
+            // Initialize an empty string
             string str = "";
+            // Loop for each word
             for (int i = 1; i <= words; i++)
             {
+                // Loop for each character
                 for (int j = 1; j <= chars; j++)
                 {
+                    // Get a random character between 'a' and 'z'
                     char c = (char)random.Next('a', 'z' + 1);
+                    // Append the character to the string
                     str += c;
                 }
+                // If the current word is not the last word, append a space to the string
                 if (i < words)
                 {
-                    str += " ";
-                }
-                else
-                {
-                    str += ".";
+                    str += ' ';
                 }
             }
+            // Set the character variable to the string
             character = str;
-            lblCharacter.Text = character;
+            // Set the character label to the character
+            lblCharacter.Text = character.ToString();
         }
 
+        // A method to start the game
         private void StartGame()
         {
+            // Disable the start button
             btnStart.Enabled = false;
+            // Enable the stop button
             btnStop.Enabled = true;
+            // Enable the input text box
             txtInput.Enabled = true;
+            // Disable the difficulty combo box
             cmbDifficulty.Enabled = false;
+            // Set the input text box to empty
             txtInput.Text = "";
+            // Set the focus to the input text box
             txtInput.Focus();
+            // Generate a new character to be entered by the user
             GenerateCharacter();
+            // Set the time variable to maxTime
             time = maxTime;
+            // Set the timer label to "Time: " + time
             lblTimer.Text = "Time: " + time;
+            // Enable the game timer
             tmrGame.Enabled = true;
         }
 
+        // A method to stop the game
         private void StopGame()
         {
+            // Disable the game timer
             tmrGame.Enabled = false;
+            // Disable the start button
             btnStart.Enabled = true;
+            // Disable the stop button
             btnStop.Enabled = false;
+            // Disable the input text box
             txtInput.Enabled = false;
+            // Enable the difficulty combo box
             cmbDifficulty.Enabled = true;
-
-            Result result = new Result()
+            // Create a new result object with the user's name, score, and difficulty level
+            Result result = new Result(InputBox("Enter your name", "Name"), score, cmbDifficulty.SelectedItem.ToString());
+            // Add the result to the best results list
+            bestResults.Add(result);
+            // Save the best results to the file
+            bestResults.Save();
+            // Reset the game variables and controls
+            ResetGame();
         }
     }
-}
