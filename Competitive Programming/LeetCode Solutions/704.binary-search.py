e01e745b1cb0a53abd21d092d8663e3be958df2c
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-11-16 19:58:32 +0200

    feat: embarrased myself not being able to write binary search anymore so moved on to arrays

---
+++ Competitive Programming/LeetCode Solutions/704.binary-search.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=704 lang=python3
+#
+# [704] Binary Search
+#
+
+
+# @lc code=start
+class Solution:
+    def search(self, nums: [int], target: int) -> int:
+        high, low = 0, len(nums)
+
+        while high <= low:
+            mid = (low + high) // 2
+            guess = nums[mid]
+
+            if guess == target:
+                return mid
+            if guess > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return -1
+
+
+# @lc code=end
