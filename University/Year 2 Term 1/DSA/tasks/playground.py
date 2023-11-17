158652e50843bb3f56fa517bfb6b80a10179feb2
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-11-17 15:16:41 +0200

    feat: something

--- University/Year 2 Term 1/DSA/tasks/playground.py
+++ University/Year 2 Term 1/DSA/tasks/playground.py
@@ -5,25 +5,40 @@ class Graph:
     def __init__(self, directed=False):
         self.graph = defaultdict(list)
         self.directed = directed
+        self.sums = defaultdict(list)
 
     def addEdge(self, u, v):
         self.graph[u].append(v)
         if not self.directed:
             self.graph[v].append(u)
 
-    def DFS(self, v, visited=None):
+    def DFS(self, v, visited=None, path=None, sum=0):
         if visited is None:
             visited = set()
-        stack = [v]
+        if path is None:
+            path = []
+        stack = [(v, visited, path, sum)]
         while stack:
-            vertex = stack.pop()
+            vertex, visited, path, sum = stack.pop()
             if vertex not in visited:
                 visited.add(vertex)
-                print(vertex, end=" ")
-                stack.extend(set(self.graph[vertex]) - visited)
+                path.append(vertex)
+                sum += vertex
+                stack.extend(
+                    (u, visited.copy(), path.copy(), sum)
+                    for u in set(self.graph[vertex]) - visited
+                )
+                self.sums[v].append((sum, path))
         return visited
 
     def displayForest(self, v):
         print("Depth-first search forest:")
         self.DFS(v)
         print()
+
+    def findPaths(self, v, target):
+        paths = []
+        for sum, path in self.sums[v]:
+            if sum == target:
+                paths.append(path)
+        return paths
