1d377fb181dbb9d086cbb8b2b88829edd915e6c2
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-11-17 09:44:23 +0200

    feat: DSA part one done

--- University/Year 2 Term 1/DSA/tasks/playground.py
+++ University/Year 2 Term 1/DSA/tasks/playground.py
@@ -1,25 +1,67 @@
-# insertion
-# deletion
-# searching
+from collections import defaultdict
 
 
-class BTreeNode:
-    def __init__(self, leaf=False):
-        self.leaf = leaf
-        self.keys = []
-        self.children = []
+class Graph:
+    def __init__(self, directed=False):
+        self.graph = defaultdict(list)
+        self.directed = directed
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+        if not self.directed:
+            self.graph[v].append(u)
+
+    def DFS(self, v, visited=None):
+        if visited is None:
+            visited = set()
+        stack = [v]
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.add(vertex)
+                print(vertex, end=" ")
+                stack.extend(set(self.graph[vertex]) - visited)
+        return visited
+
+    def displayForest(self, v):
+        print("Depth-first search forest:")
+        self.DFS(v)
+        print()
 
 
-class BSTNode:
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
+def menu():
+    print()
+    print("1. Create a new graph")
+    print("2. Add an edge")
+    print("3. Perform depth-first search")
+    print("4. Display depth-first search forest")
+    print("5. Exit")
+    choice = input("Enter your choice: ")
+    print()
+    return choice
 
 
-class BTree:
-    pass
+def main():
+    g = None
+    while True:
+        choice = menu()
+        if choice == "1":
+            directed = input("Is the graph directed? (y/n): ") == "y"
+            g = Graph(directed)
+        elif choice == "2":
+            u = int(input("Enter the first vertex: "))
+            v = int(input("Enter the second vertex: "))
+            g.addEdge(u, v)
+        elif choice == "3":
+            v = int(input("Enter the starting vertex: "))
+            g.DFS(v)
+            print()
+        elif choice == "4":
+            v = int(input("Enter the starting vertex: "))
+            print()
+            g.displayForest(v)
+        else:
+            break
 
 
-class BST:
-    pass
+main()
