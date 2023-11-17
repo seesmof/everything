c0d5e4112aec33e6bbdb0d38e08185155b68d848
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-11-17 10:23:48 +0200

    feat: starting the stuff yee yee

--- University/Year 2 Term 1/DSA/tasks/playground.py
+++ University/Year 2 Term 1/DSA/tasks/playground.py
@@ -1,4 +1,4 @@
-from collections import defaultdict, deque
+from collections import defaultdict
 
 
 class Graph:
@@ -11,51 +11,19 @@ class Graph:
         if not self.directed:
             self.graph[v].append(u)
 
-    def BFS(self, start):
-        visited = set()
-        queue = deque([start])
-
-        while queue:
-            vertex = queue.popleft()
+    def DFS(self, v, visited=None):
+        if visited is None:
+            visited = set()
+        stack = [v]
+        while stack:
+            vertex = stack.pop()
             if vertex not in visited:
                 visited.add(vertex)
-                queue.extend(set(self.graph[vertex]) - visited)
-
+                print(vertex, end=" ")
+                stack.extend(set(self.graph[vertex]) - visited)
         return visited
 
-    def displayTree(self, start):
-        visited = set()
-        queue = deque([start])
-        tree = defaultdict(list)
-
-        while queue:
-            vertex = queue.popleft()
-            if vertex not in visited:
-                visited.add(vertex)
-                for neighbor in self.graph[vertex]:
-                    if neighbor not in visited:
-                        tree[vertex].append(neighbor)
-                        queue.append(neighbor)
-
-        return tree
-
-    def displayResults(self, start):
-        visited = self.BFS(start)
-        print(f"BFS traversal: {visited}")
-
-
-g = Graph()
-
-# Add edges to the graph
-g.addEdge(1, 2)
-g.addEdge(1, 3)
-g.addEdge(2, 4)
-g.addEdge(2, 5)
-g.addEdge(3, 6)
-g.addEdge(3, 7)
-# Perform a BFS traversal starting from vertex 1
-g.displayResults(1)
-# Display the BFS tree
-tree = dict(g.displayTree(1))
-for key, value in tree.items():
-    print(f"{key}: {value}")
+    def displayForest(self, v):
+        print("Depth-first search forest:")
+        self.DFS(v)
+        print()
