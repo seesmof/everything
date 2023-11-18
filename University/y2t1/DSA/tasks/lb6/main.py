b816d2e51bafd0dedf8648f8972f276c5087452f
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-11-18 15:29:26 +0200

    feat: okay time to do something else

--- University/y2t1/DSA/tasks/lb6/main.py
+++ University/y2t1/DSA/tasks/lb6/main.py
@@ -9,71 +9,72 @@
 GRAPH_FILE_PATH = "D:/code/everything/University/y2t1/DSA/tasks/lb6/input.txt"
 
 
-def dijkstra():
+def algorithms():
     from collections import defaultdict, deque
+    import heapq
 
     class Graph:
         def __init__(self, directed=False):
             self.graph = defaultdict(list)
             self.directed = directed
 
-        def addEdge(self, u, v):
-            self.graph[u].append(v)
+        def addEdge(self, u, v, w):
+            self.graph[u].append((v, w))
             if not self.directed:
-                self.graph[v].append(u)
-
-        def BFS(self, start):
-            visited = set()
-            queue = deque([start])
-
-            while queue:
-                vertex = queue.popleft()
-                if vertex not in visited:
-                    visited.add(vertex)
-                    queue.extend(set(self.graph[vertex]) - visited)
-
-            return visited
-
-        def displayTree(self, start):
-            visited = set()
-            queue = deque([start])
-            tree = defaultdict(list)
-
-            while queue:
-                vertex = queue.popleft()
-                if vertex not in visited:
-                    visited.add(vertex)
-                    for neighbor in self.graph[vertex]:
-                        if neighbor not in visited:
-                            tree[vertex].append(neighbor)
-                            queue.append(neighbor)
-
-            return tree
-
-        def displayResults(self, start):
-            visited = self.BFS(start)
-            print(f"BFS traversal: {visited}")
+                self.graph[v].append((u, w))
 
         def displayGraph(self):
             for key, value in self.graph.items():
-                print(f"{key} 🔗 {', '.join(map(str, value))}")
+                if self.directed:
+                    print(f"{key} → {', '.join(map(str, value))}")
+                else:
+                    print(f"{key} 🔗 {', '.join(map(str, value))}")
 
         def loadFromFile(self, filename):
             with open(filename, "r") as file:
                 for line in file:
-                    u, v = line.strip().split()
-                    self.addEdge(int(u), int(v))
+                    try:
+                        u, v, w = line.strip().split()
+                        self.addEdge(int(u), int(v), int(w))
+                    except ValueError:
+                        print(f"Skipping line {line}")
+
+        def dfs(self, start):
+            visited = set()
+            stack = [start]
+            while stack:
+                vertex = stack.pop()
+                if vertex not in visited:
+                    visited.add(vertex)
+                    stack.extend(set(self.graph[vertex]) - visited)
+            return visited
+
+        def dijkstra(self, start):
+            distances = {vertex: float("inf") for vertex in self.graph}
+            distances[start] = 0
+            queue = [(0, start)]
+            while queue:
+                currentDistance, currentVertex = heapq.heappop(queue)
+                if currentDistance > distances[currentVertex]:
+                    continue
+                for neighbor, weight in self.graph[currentVertex]:
+                    distance = currentDistance + weight
+                    if distance < distances[neighbor]:
+                        distances[neighbor] = distance
+                        heapq.heappush(queue, (distance, neighbor))
+            return distances
 
     def main():
         g = None
         while True:
-            print("\nBFS")
+            print("\nALGORITHMS")
             print("1. Create a new graph")
             print("2. Add an edge")
             print("3. Display graph")
-            print("4. Perform breadth-first search")
-            print("5. Display breadth-first search tree")
-            print("6. Exit")
+            print("4. Dijkstra")
+            print("5. Floyd-Warshell")
+            print("6. Bellman-Ford")
+            print("7. Exit")
             choice = int(input(": "))
             print()
 
@@ -102,12 +103,8 @@ def dijkstra():
 
             elif choice == 4:
                 v = int(input("Enter the starting vertex: "))
-                g.displayResults(v)
-
-            elif choice == 5:
-                v = int(input("Enter the starting vertex: "))
                 print()
-                tree = dict(g.displayTree(v))
+                tree = dict(g.dijkstra(v))
                 for key, value in tree.items():
                     print(f"{key}: {value}")
 
@@ -140,7 +137,7 @@ def menu():
         choice = int(input(": "))
 
         if choice == 1:
-            dijkstra()
+            algorithms()
         elif choice == 2:
             project_minimal_times()
         elif choice == 3:
