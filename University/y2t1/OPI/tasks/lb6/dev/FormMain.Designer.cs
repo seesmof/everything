6a3352c0a05454fe13b954182cc9b43d0dab6201
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-11-18 16:05:38 +0200

    feat: SHIP IT DOG

--- University/y2t1/OPI/tasks/lb6/dev/FormMain.Designer.cs
+++ University/y2t1/OPI/tasks/lb6/dev/FormMain.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace dev
+﻿// Task C - Designer
+namespace dev
 {
     partial class FormMain
     {
@@ -28,192 +29,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.lblCharacter = new System.Windows.Forms.Label();
-            this.txtInput = new System.Windows.Forms.TextBox();
-            this.lblTimer = new System.Windows.Forms.Label();
-            this.tmrGame = new System.Windows.Forms.Timer(this.components);
-            this.btnStart = new System.Windows.Forms.Button();
-            this.btnStop = new System.Windows.Forms.Button();
-            this.cmbDifficulty = new System.Windows.Forms.ComboBox();
-            this.lblScore = new System.Windows.Forms.Label();
-            this.dgvScores = new System.Windows.Forms.DataGridView();
-            this.ResultName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ResultScore = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ResultDifficulty = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvScores)).BeginInit();
+            this.labelKeyCode = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // lblCharacter
+            // labelKeyCode
             // 
-            this.lblCharacter.AutoSize = true;
-            this.lblCharacter.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCharacter.Location = new System.Drawing.Point(12, 48);
-            this.lblCharacter.Name = "lblCharacter";
-            this.lblCharacter.Size = new System.Drawing.Size(12, 18);
-            this.lblCharacter.TabIndex = 1;
-            this.lblCharacter.Text = "f";
-            // 
-            // txtInput
-            // 
-            this.txtInput.Location = new System.Drawing.Point(15, 78);
-            this.txtInput.Name = "txtInput";
-            this.txtInput.Size = new System.Drawing.Size(406, 22);
-            this.txtInput.TabIndex = 2;
-            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
-            // 
-            // lblTimer
-            // 
-            this.lblTimer.AutoSize = true;
-            this.lblTimer.Location = new System.Drawing.Point(12, 20);
-            this.lblTimer.Name = "lblTimer";
-            this.lblTimer.Size = new System.Drawing.Size(51, 16);
-            this.lblTimer.TabIndex = 3;
-            this.lblTimer.Text = "Time: 0";
-            // 
-            // btnStart
-            // 
-            this.btnStart.BackColor = System.Drawing.Color.ForestGreen;
-            this.btnStart.FlatAppearance.BorderSize = 0;
-            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStart.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnStart.Location = new System.Drawing.Point(427, 78);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(77, 22);
-            this.btnStart.TabIndex = 4;
-            this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = false;
-            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
-            // 
-            // btnStop
-            // 
-            this.btnStop.BackColor = System.Drawing.Color.DarkRed;
-            this.btnStop.FlatAppearance.BorderSize = 0;
-            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStop.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnStop.Location = new System.Drawing.Point(510, 78);
-            this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(62, 22);
-            this.btnStop.TabIndex = 4;
-            this.btnStop.Text = "Stop";
-            this.btnStop.UseVisualStyleBackColor = false;
-            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
-            // 
-            // cmbDifficulty
-            // 
-            this.cmbDifficulty.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbDifficulty.FormattingEnabled = true;
-            this.cmbDifficulty.Items.AddRange(new object[] {
-            "Easy",
-            "Medium",
-            "High",
-            "Expert",
-            "Master"});
-            this.cmbDifficulty.Location = new System.Drawing.Point(427, 20);
-            this.cmbDifficulty.Name = "cmbDifficulty";
-            this.cmbDifficulty.Size = new System.Drawing.Size(145, 24);
-            this.cmbDifficulty.TabIndex = 5;
-            this.cmbDifficulty.SelectedIndexChanged += new System.EventHandler(this.cmbDifficulty_SelectedIndexChanged);
-            // 
-            // lblScore
-            // 
-            this.lblScore.AutoSize = true;
-            this.lblScore.Location = new System.Drawing.Point(69, 20);
-            this.lblScore.Name = "lblScore";
-            this.lblScore.Size = new System.Drawing.Size(56, 16);
-            this.lblScore.TabIndex = 3;
-            this.lblScore.Text = "Score: 0";
-            // 
-            // dgvScores
-            // 
-            this.dgvScores.AllowUserToAddRows = false;
-            this.dgvScores.AllowUserToDeleteRows = false;
-            this.dgvScores.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dgvScores.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.dgvScores.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.dgvScores.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
-            this.dgvScores.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.ControlLightLight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvScores.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
-            this.dgvScores.ColumnHeadersHeight = 28;
-            this.dgvScores.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.ResultName,
-            this.ResultScore,
-            this.ResultDifficulty});
-            this.dgvScores.EnableHeadersVisualStyles = false;
-            this.dgvScores.Location = new System.Drawing.Point(15, 118);
-            this.dgvScores.Name = "dgvScores";
-            this.dgvScores.ReadOnly = true;
-            this.dgvScores.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            this.dgvScores.RowHeadersVisible = false;
-            this.dgvScores.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvScores.Size = new System.Drawing.Size(557, 231);
-            this.dgvScores.TabIndex = 6;
-            // 
-            // ResultName
-            // 
-            this.ResultName.HeaderText = "Name";
-            this.ResultName.Name = "ResultName";
-            this.ResultName.ReadOnly = true;
-            // 
-            // ResultScore
-            // 
-            this.ResultScore.HeaderText = "Score";
-            this.ResultScore.Name = "ResultScore";
-            this.ResultScore.ReadOnly = true;
-            // 
-            // ResultDifficulty
-            // 
-            this.ResultDifficulty.HeaderText = "Difficulty";
-            this.ResultDifficulty.Name = "ResultDifficulty";
-            this.ResultDifficulty.ReadOnly = true;
+            this.labelKeyCode.AutoSize = true;
+            this.labelKeyCode.Location = new System.Drawing.Point(12, 9);
+            this.labelKeyCode.Name = "labelKeyCode";
+            this.labelKeyCode.Size = new System.Drawing.Size(141, 16);
+            this.labelKeyCode.TabIndex = 0;
+            this.labelKeyCode.Text = "Waiting for key press...";
             // 
             // FormMain
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(584, 361);
-            this.Controls.Add(this.dgvScores);
-            this.Controls.Add(this.cmbDifficulty);
-            this.Controls.Add(this.btnStop);
-            this.Controls.Add(this.btnStart);
-            this.Controls.Add(this.lblScore);
-            this.Controls.Add(this.lblTimer);
-            this.Controls.Add(this.txtInput);
-            this.Controls.Add(this.lblCharacter);
+            this.Controls.Add(this.labelKeyCode);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximumSize = new System.Drawing.Size(600, 400);
             this.MinimumSize = new System.Drawing.Size(600, 400);
             this.Name = "FormMain";
             this.Text = "Робота з клавіатурою - ОПІ Лабораторна 6";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvScores)).EndInit();
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.FormMain_KeyPress);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label lblCharacter;
-        private System.Windows.Forms.TextBox txtInput;
-        private System.Windows.Forms.Label lblTimer;
-        private System.Windows.Forms.Timer tmrGame;
-        private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.Button btnStop;
-        private System.Windows.Forms.ComboBox cmbDifficulty;
-        private System.Windows.Forms.Label lblScore;
-        private System.Windows.Forms.DataGridView dgvScores;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ResultName;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ResultScore;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ResultDifficulty;
+
+        private System.Windows.Forms.Label labelKeyCode;
     }
 }
 
