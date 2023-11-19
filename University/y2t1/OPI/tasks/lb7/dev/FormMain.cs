660d7f10af174044f40c1d285ce1a918be07c5b0
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-11-19 11:38:56 +0200

    feat: OPI Lab 7 - got the interface down, got the line working, but it draws lotsa lines and i just want one

---
+++ University/y2t1/OPI/tasks/lb7/dev/FormMain.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dev
+{
+    public partial class FormMain : Form
+    {
+        string currentTool = null;
+        Point startPoint;
+        Graphics g;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void selectTool(object sender, EventArgs e)
+        {
+            ToolStripButton button = (ToolStripButton)sender;
+            currentTool = button.Tag.ToString();
+        }
+
+        private void toolStripButtonLine_Click(object sender, EventArgs e)
+        {
+            selectTool(sender, e);
+        }
+
+        private void toolStripButtonEllipse_Click(object sender, EventArgs e)
+        {
+            selectTool(sender, e);
+        }
+
+        private void toolStripButtonPencil_Click(object sender, EventArgs e)
+        {
+            selectTool(sender, e);
+        }
+
+        private void toolStripButtonRectangle_Click(object sender, EventArgs e)
+        {
+            selectTool(sender, e);
+        }
+
+        private void toolStripButtonText_Click(object sender, EventArgs e)
+        {
+            selectTool(sender, e);
+        }
+
+        private void drawingCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (currentTool == "Line")
+            {
+                g = drawingCanvas.CreateGraphics();
+                g.Clear(Color.White);
+                startPoint = e.Location;
+            }
+        }
+
+        private void drawingCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (currentTool == "Line" && g != null)
+            {
+                g.DrawLine(Pens.Black, startPoint, e.Location);
+            }
+        }
+
+        private void drawingCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (currentTool == "Line" && g != null)
+            {
+                g.DrawLine(Pens.Black, startPoint, e.Location);
+                g.Dispose();
+                g = null;
+            }
+        }
+    }
+}
