f848e1e43b1af287af6baee267c4caca3a4546d5
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-11-19 22:16:11 +0200

    feat: navbar done, thank you Goodness

--- University/y2t1/WEB/tasks/ct1/code/app/layout.jsx
+++ University/y2t1/WEB/tasks/ct1/code/app/layout.jsx
@@ -1,18 +1,24 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Flickster",
   description:
-    "App for movie enthusiast that aims to streamline the process of choosing and tracking movies and TV shows.",
+    "An app for movie enthusiasts that aims to streamline movie finding and picking process.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="min-h-screen bg-slate-900 text-slate-100">
+          <Navbar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
---
+++ University/y2t1/WEB/tasks/ct1/code/components/Navbar.jsx
@@ -0,0 +1,65 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="bg-slate-800 fixed bottom-0 left-0 right-0 md:sticky md:top-0">
+        <div className="flex items-center justify-between p-2 gap-2 md:hidden">
+          <button className="p-2 grow rounded-md hover:bg-slate-700 active:bg-slate-600 duration-300">
+            Home
+          </button>
+          <button className="p-2 grow rounded-md hover:bg-slate-700 active:bg-slate-600 duration-300">
+            Catalog
+          </button>
+          <button className="p-2 grow rounded-md hover:bg-slate-700 active:bg-slate-600 duration-300">
+            Movies
+          </button>
+          <button className="p-2 grow rounded-md hover:bg-slate-700 active:bg-slate-600 duration-300">
+            Shows
+          </button>
+        </div>
+        <div className="hidden md:flex max-w-6xl mx-auto justify-between items-center p-4">
+          <Link
+            href={"/"}
+            className="font-medium text-xl duration-300 text-white hover:text-slate-200 active:text-slate-300"
+          >
+            Flickster
+          </Link>
+          <div className="flex items-center justify-center gap-4">
+            <Link
+              className="hover:text-slate-200 active:text-slate-300 duration-300"
+              href={"/"}
+            >
+              Home
+            </Link>
+            <Link
+              className="hover:text-slate-200 active:text-slate-300 duration-300"
+              href={"/catalog"}
+            >
+              Catalog
+            </Link>
+            <Link
+              className="hover:text-slate-200 active:text-slate-300 duration-300"
+              href={"/movies"}
+            >
+              Movies
+            </Link>
+            <Link
+              className="hover:text-slate-200 active:text-slate-300 duration-300"
+              href={"/shows"}
+            >
+              TV Shows
+            </Link>
+          </div>
+          <input
+            type="text"
+            className="bg-inherit rounded-md border-2 p-1 px-4 border-slate-700 hover:border-slate-500 duration-300 max-w-[16rem]"
+            placeholder="Search..."
+          />
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
