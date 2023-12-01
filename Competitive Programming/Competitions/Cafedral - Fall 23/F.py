68d6950a100fed29619ec2aa091c9c4e57c75ab2
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-12-01 15:45:38 +0200

    feat: okay i did everything i could. 4 solved, thanks God

--- Competitive Programming/Competitions/Cafedral - Fall 23/F.py
+++ Competitive Programming/Competitions/Cafedral - Fall 23/F.py
@@ -1,3 +1,13 @@
+"""
+Find such x, so that x^2+sqrt(x)=C with an accuracy of at least 6 decimal points.
+
+Input: One real number C
+
+Output: Print the desired root x with at least 6 digits after the decimal point.
+
+solve in python in as little code as possible
+"""
+
 from math import sqrt
 
 
