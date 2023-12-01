da5c124b4b7ff9ce97e5f9d3ebe2e1db54620e17
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-12-01 15:25:02 +0200

    feat: e fail

---
+++ Competitive Programming/Competitions/Cafedral - Fall 23/E.py
@@ -0,0 +1,13 @@
+n = int(input())
+cubes = list(input())
+
+if n % 2 == 0:
+    print("Ok")
+else:
+    counts = {cube: 0 for cube in cubes}
+    for cube in cubes:
+        counts[cube] += 1
+
+    for cube, count in counts.items():
+        if count == 1:
+            print(cube)
