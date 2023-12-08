9e43d26e3f8aff76d4b6b6566e8c6d9e92471473
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-12-08 11:34:16 +0200

    feat: GOD PLEASE HELP

--- University/y2t1/VM/tasks/test _ dec-8/answers.py
+++ University/y2t1/VM/tasks/test _ dec-8/answers.py
@@ -1,19 +1,10 @@
-import sympy as sp
+from sympy import symbols, limit, oo
 
-n = sp.symbols("n")
-term = 1 / sp.sqrt(2 * n + 1)
-
-# Check if the terms decrease monotonically
-prev_term = term.subs(n, 0)
-for i in range(1, 100):
-    next_term = term.subs(n, i)
-    if next_term > prev_term:
-        print("The series does not decrease monotonically.")
-        break
-    prev_term = next_term
-else:
-    print("The series decreases monotonically.")
-
-# Check if the limit of the terms is zero
-limit = sp.limit(term, n, sp.oo)
-print("The limit of the terms as n approaches infinity is", limit)
+n = symbols("n", integer=True)
+x = symbols("x", real=True)
+a = (x - 4) ** n / (n * 2**n)
+RatioTest = abs(a.subs(n, n + 1) / a)
+L = limit(RatioTest, n, oo)
+a_left = a.subs(x, 4)
+print(L)
+print(a_left)
