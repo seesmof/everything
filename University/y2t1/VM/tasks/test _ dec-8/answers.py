1a9aec4e2114ec7e5b138b531dc6a0bbf9672cc4
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-12-08 10:49:12 +0200

    feat: 9 GOD PLEASE

--- University/y2t1/VM/tasks/test _ dec-8/answers.py
+++ University/y2t1/VM/tasks/test _ dec-8/answers.py
@@ -1,26 +1,17 @@
-from sympy import symbols, limit, Abs, oo, Sum, factorial, S
+from sympy import symbols, diff, sin, cos, factorial
 
-# Define the variable and the sequence
+# Define the variable
 x = symbols('x')
-n = symbols('n', integer=True)
-a_n = ((x-4)**n) / (n * 2**n)
 
-# Calculate the ratio a_n / a_{n+1}
-ratio = Abs(a_n / a_n.subs(n, n+1))
+# Define the function
+f = (sin(x)**2) / (cos(x)**2)
 
-# Calculate the limit as n approaches infinity
-R = limit(ratio, n, oo)
+# Calculate the first and second derivatives at 0
+f_prime_0 = diff(f, x).subs(x, 0)
+f_double_prime_0 = diff(f, x, 2).subs(x, 0)
 
-# Check the convergence at the left endpoint x = 4 - R
-left_endpoint = 4 - R
-series_at_left = a_n.subs(x, left_endpoint)
-abs_series_at_left = Abs(a_n).subs(x, left_endpoint)
+# Calculate the coefficients c_1 and c_2
+c_1 = f_prime_0 / factorial(1)
+c_2 = f_double_prime_0 / factorial(2)
 
-# Check if the series converges absolutely at the left endpoint
-abs_convergence_at_left = Sum(abs_series_at_left, (n, 0, oo)).is_convergent()
-
-# Check if the series converges conditionally at the left endpoint
-conditional_convergence_at_left = Sum(series_at_left, (n, 0, oo)).is_convergent() and not abs_convergence_at_left
-
-print(R, abs_convergence_at_left, conditional_convergence_at_left
-)
+print(c_1, c_2, c_1 + c_2)
