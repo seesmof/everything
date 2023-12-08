c0e1bd70c5abb9d90736740191ae282b36e967b3
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-12-08 10:55:27 +0200

    feat: 9 YES

--- University/y2t1/VM/tasks/test _ dec-8/answers.py
+++ University/y2t1/VM/tasks/test _ dec-8/answers.py
@@ -1,17 +1,14 @@
-from sympy import symbols, diff, sin, cos, factorial
+import sympy as sp
 
-# Define the variable
-x = symbols('x')
+x = sp.symbols("x")
+f = sp.tan(x) ** 2
 
-# Define the function
-f = (sin(x)**2) / (cos(x)**2)
+# calculate the first two derivatives
+f1 = sp.diff(f, x)
+f2 = sp.diff(f1, x)
 
-# Calculate the first and second derivatives at 0
-f_prime_0 = diff(f, x).subs(x, 0)
-f_double_prime_0 = diff(f, x, 2).subs(x, 0)
+# evaluate the derivatives at 0
+c1 = f1.subs(x, 0)
+c2 = f2.subs(x, 0) / 2  # divide by 2! to get the coefficient
 
-# Calculate the coefficients c_1 and c_2
-c_1 = f_prime_0 / factorial(1)
-c_2 = f_double_prime_0 / factorial(2)
-
-print(c_1, c_2, c_1 + c_2)
+print(c1 + c2)
