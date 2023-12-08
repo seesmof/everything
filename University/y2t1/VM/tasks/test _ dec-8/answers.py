11eb7b62039253dff6e0e077e552fe0b043387c5
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-12-08 11:24:45 +0200

    feat: 5 GOD YES PLEASE

--- University/y2t1/VM/tasks/test _ dec-8/answers.py
+++ University/y2t1/VM/tasks/test _ dec-8/answers.py
@@ -1,7 +1,19 @@
 import sympy as sp
 
 n = sp.symbols("n")
-a_n = (2**n + 3) / (3**n + 2)
-L = sp.limit(a_n ** (1 / n), n, sp.oo)
+term = 1 / sp.sqrt(2 * n + 1)
 
-print(L)
+# Check if the terms decrease monotonically
+prev_term = term.subs(n, 0)
+for i in range(1, 100):
+    next_term = term.subs(n, i)
+    if next_term > prev_term:
+        print("The series does not decrease monotonically.")
+        break
+    prev_term = next_term
+else:
+    print("The series decreases monotonically.")
+
+# Check if the limit of the terms is zero
+limit = sp.limit(term, n, sp.oo)
+print("The limit of the terms as n approaches infinity is", limit)
