f34c846f167ddbd9af5cb801ca80a67cd378bb4b
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-12-19 12:47:04 +0200

    feat: okay

--- Competitive Programming/LeetCode Solutions/21.merge-two-sorted-lists.py
+++ Competitive Programming/LeetCode Solutions/21.merge-two-sorted-lists.py
@@ -18,33 +18,9 @@ class ListNode:
 
 
 class Solution:
-    def calculateLength(self, list: [ListNode]) -> int:
-        tmp = list
-        count = 1
-        while tmp.next != None:
-            tmp = tmp.next
-            count += 1
-        return count
-
     def mergeTwoLists(self, list1: [ListNode], list2: [ListNode]) -> [ListNode]:
-        lengthOne, lengthTwo = self.calculateLength(list1), self.calculateLength(list2)
-        nodeOne, nodeTwo = list1, list2
-
-        while nodeTwo:
-            oneValue, twoValue = nodeOne.val, nodeTwo.val
-            print(oneValue, twoValue)
-
-            if nodeOne.next:
-                nodeOne = nodeOne.next
-            else:
-                nodeOne = nodeTwo
-
-            if nodeTwo.next:
-                nodeTwo = nodeTwo.next
-            else:
-                break
-
-        return nodeOne
+        res = None
+        curr = list1
 
 
 # @lc code=end
