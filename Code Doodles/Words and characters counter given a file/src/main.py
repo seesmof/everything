3bd4e5833ff514333ff6dfeab08455b376c103f2
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-12-21 11:27:46 +0200

    feat: okay okay

--- Code Doodles/Words and characters counter given a file/src/main.py
+++ Code Doodles/Words and characters counter given a file/src/main.py
@@ -4,6 +4,7 @@ from rich.table import Table
 from rich.traceback import install
 from rich.markdown import Markdown as md
 from customtkinter import *
+import threading
 
 from components.AlertPopup import AlertPopup
 from util.main import *
@@ -13,7 +14,7 @@ console = Console()
 
 
 app = CTk()
-app.geometry("380x400")
+app.geometry("380x560")
 app.resizable(False, False)
 app.title("Count Words and Characters")
 app.bind("<Escape>", lambda event: closeApp(app, event=event))
@@ -75,15 +76,64 @@ def countWords(lines):
     return res
 
 
+def getMostPopularWords(lines):
+    words = {}
+    for line in lines:
+        wordsInLine = line.split(" ")
+        for word in wordsInLine:
+            if (
+                word == ""
+                or word == " "
+                or word == "."
+                or word == "!"
+                or word == "?"
+                or word == "("
+                or word == ")"
+                or word == ","
+                or word == "~"
+                or word == "-"
+                or word == "'"
+                or word == '"'
+                or word == "”"
+                or word == "“"
+                or word == "’"
+                or word == "+"
+            ):
+                continue
+
+            if word in words:
+                words[word] += 1
+            else:
+                words[word] = 1
+    return words
+
+
 def renderFromTextTab(root):
+    def updateMostPopularWords(words, mostPopularWordsFrame):
+        for widget in mostPopularWordsFrame.winfo_children():
+            widget.destroy()
+
+        words = {
+            k: v
+            for k, v in sorted(words.items(), key=lambda item: item[1], reverse=True)
+        }
+        for word in words:
+            CTkLabel(
+                mostPopularWordsFrame,
+                text=f"{word}: {words[word]}",
+                font=("Arial", 12),
+            ).pack(padx=5, anchor="w")
+
     def performCalculations(data):
         lines, linesCount = removeWhiteSpace(data)
         symbolsCount = countSymbols(lines)
         wordsCount = countWords(lines)
+        mostPopularWords = getMostPopularWords(lines[:40])
 
         resultsLines.configure(text=f"Lines: {linesCount}")
         resultsSymbols.configure(text=f"Symbols: {symbolsCount}")
         resultsWords.configure(text=f"Words: {wordsCount}")
+        updateMostPopularWords(mostPopularWords, mostPopularWordsTable)
 
     getTextHeading = CTkLabel(
         root, text="Enter text you want to count words in", font=("Arial", 14, "bold")
@@ -97,23 +147,24 @@ def renderFromTextTab(root):
         lambda event: performCalculations(getTextInput.get("0.0", "end")),
     )
 
-    performCountButton = CTkButton(
-        root,
-        text="Perform Count",
-        width=360,
-        font=("Arial", 12, "bold"),
-        command=lambda: performCalculations(getTextInput.get("0.0", "end")),
-    )
-    performCountButton.place(x=0, y=190)
-
     resultsLines = CTkLabel(root, text="", font=("Arial", 13))
-    resultsLines.place(x=0, y=230)
+    resultsLines.place(x=0, y=190)
 
     resultsSymbols = CTkLabel(root, text="", font=("Arial", 13))
-    resultsSymbols.place(x=0, y=260)
+    resultsSymbols.place(x=100, y=190)
 
     resultsWords = CTkLabel(root, text="", font=("Arial", 13))
-    resultsWords.place(x=0, y=290)
+    resultsWords.place(x=240, y=190)
+
+    mostPopularWordsTable = CTkScrollableFrame(
+        root,
+        width=335,
+        border_color="#333333",
+        border_width=2,
+        label_text="Most popular words",
+        label_font=("Arial", 12, "bold"),
+    )
+    mostPopularWordsTable.place(x=0, y=230)
 
 
 renderFromTextTab(fromTextTab)
