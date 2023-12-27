365b82ab93a51063f7236bfcf671d1dee08d49e2
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-12-27 02:25:13 +0200

    feat: FUCK YOU!

--- Code Doodles/Spotify Playlist Songs Downloader/whatup.py
+++ Code Doodles/Spotify Playlist Songs Downloader/whatup.py
@@ -1,12 +1,39 @@
+from time import sleep
 from rich.console import Console
 from rich.traceback import install
 
 install()
 console = Console()
 
-import YouTubeMusicAPI
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-songName = "Ridin' Blessed"
-result = YouTubeMusicAPI.search(songName)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-console.print(result["url"] if result else "[red]No results found[/]")
+driver.get("https://y2mate.com.co/en185z/youtube-to-mp3-converter")
+
+inputBox = driver.find_element(by="id", value="txt-url")
+inputBox.send_keys("https://music.youtube.com/watch?v=3mPu1ho7ThY")
+
+searchButton = driver.find_element(by="id", value="btn-submit")
+searchButton.click()
+
+sleep(4)
+
+downloadButton = driver.find_element(
+    by="xpath", value="//a[contains(., 'Download') and @data-ftype=\"mp3\"]"
+)
+neededUrl = downloadButton.get_attribute("href")
+
+driver.execute_script("window.open()")
+driver.switch_to.window(driver.window_handles[-1])
+driver.get(neededUrl)
+
+WebDriverWait(driver, 12).until(
+    EC.presence_of_element_located(
+        ("xpath", "//a[contains(., 'Download') and @id=\"A_downloadUrl\"")
+    )
+).click()
