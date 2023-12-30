b1cbf7e9923fa5f2d144f31651bac92921205436
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-12-30 11:27:54 +0200

    chore:

--- Winter of Code - 2023-2024/Project Ideas Manager/src/main.py
+++ Winter of Code - 2023-2024/Project Ideas Manager/src/main.py
@@ -15,9 +15,25 @@ console = Console()
 
 from click_shell import shell
 import sqlite3
+from enum import Enum
+
+
+class Status(Enum):
+    TODO = "todo"
+    DOING = "doing"
+    DONE = "done"
+
+
+class Difficulty(Enum):
+    EASY = "easy"
+    MEDIUM = "medium"
+    HARD = "hard"
+
 
 currentDir = path.dirname(path.abspath(__file__))
 databasePath = path.join(currentDir, "..", "data", "project_ideas.db")
+conn = sqlite3.connect(databasePath)
+c = conn.cursor()
 
 # help
 # add task
@@ -48,19 +64,79 @@ show: Show all project ideas
 
 
 def getNewIdeaData():
-    name = input("Project Name *: ")
-    description = input("Project Description: ")
-    status = input("Project Status: ")
-    difficulty = input("Project Difficulty: ")
+    console.print()
+    name = input("Project Name - Required\n: ")
+    description = input("Project Description - Can be empty\n: ")
+    status = input("Project Status - todo | doing | done. Default is todo\n: ")
+    difficulty = input("Project Difficulty - easy | medium | hard. Default is easy\n: ")
+    console.print()
 
+    name = name if name else None
     description = description if description else None
-    status = status if status else "todo"
-    difficulty = difficulty if difficulty else 1
+    status = Status(status) if status in [s.value for s in Status] else Status.TODO
+    difficulty = (
+        Difficulty(difficulty)
+        if difficulty in [d.value for d in Difficulty]
+        else Difficulty.EASY
+    )
+
+    return name, description, status, difficulty
+
+
+def createTable():
+    c.execute(
+        "CREATE TABLE IF NOT EXISTS project_ideas (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, status TEXT, difficulty TEXT)"
+    )
+    conn.commit()
+
+
+def addTask(name, description, status, difficulty):
+    c.execute(
+        "INSERT INTO project_ideas (name, description, status, difficulty) VALUES (?, ?, ?, ?)",
+        (name, description, status.value, difficulty.value),
+    )
+    conn.commit()
 
 
 @pm_shell.command()
 def add():
     name, description, status, difficulty = getNewIdeaData()
+    if not name:
+        console.print("Name cannot be empty")
+        return
+
+    createTable()
+    addTask(name, description, status, difficulty)
+    console.print("Task added successfully")
+
+
+def showTasks():
+    try:
+        c.execute("SELECT * FROM project_ideas")
+    except sqlite3.OperationalError:
+        createTable()
+    rows = c.fetchall()
+    if not rows:
+        console.print("No project ideas found. Create one with 'add'")
+        return
+
+    t = Table("ID", "Name", "Description", "Status", "Difficulty")
+
+    for row in rows:
+        t.add_row(
+            str(row[0]),
+            str(row[1]),
+            str(row[2]),
+            str(row[3]),
+            str(row[4]),
+        )
+
+    console.print(t)
+
+
+@pm_shell.command()
+def show():
+    showTasks()
 
 
 if __name__ == "__main__":
