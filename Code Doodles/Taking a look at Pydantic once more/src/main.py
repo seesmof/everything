b9288d6a3f300d5598e2fbdfde5f5e65a2b6093c
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-01-02 21:02:47 +0200

    feat: validation yep

--- Code Doodles/Taking a look at Pydantic once more/src/main.py
+++ Code Doodles/Taking a look at Pydantic once more/src/main.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from rich.console import Console
 from rich.traceback import install
 
@@ -12,6 +12,11 @@ class Variant(BaseModel):
     variantCode: str
     available: bool
     price: int
+
+    @field_validator("variantCode")
+    def validate_variant_code(cls, v):
+        if len(v) != 1:
+            raise ValueError("Variant code must be one character long")
 
 
 class Product(BaseModel):
@@ -31,3 +36,14 @@ item = Product(
     ],
 )
 console.print(item)
+
+newItem = Product(
+    id=51982,
+    title="New Sneakers",
+    variants=[
+        Variant(name="Small", variantCode="S", available=True, price=10),
+        Variant(name="Medium", variantCode="M", available=True, price=20),
+        Variant(name="Large", variantCode="L", available=False, price=30),
+        Variant(name="Extra Large", variantCode="X", available=True, price=40),
+    ],
+)
