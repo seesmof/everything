8dfdc84d167c6fc0a6c8de827651251f956a5ea4
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-01-03 10:30:33 +0200

    feat: okay its easy. like binary search pretty much

--- Competitive Programming/playground.py
+++ Competitive Programming/playground.py
@@ -1,1 +1,15 @@
+from typing import List
 from Archive.testing_imports import *
+
+
+def solve(numbers: List[int], target: int) -> List[int]:
+    left, right = 0, len(numbers) - 1
+    while left < right:
+        sum = numbers[left] + numbers[right]
+        if sum == target:
+            return [left + 1, right + 1]
+        elif sum < target:
+            left += 1
+        else:
+            right -= 1
+    return []
