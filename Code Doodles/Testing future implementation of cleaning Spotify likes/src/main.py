bd2960952c6c8c7d562f4da171546ec9056bfca1
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-01-08 11:51:01 +0200

    feat: okay

--- Code Doodles/Testing future implementation of cleaning Spotify likes/src/main.py
+++ Code Doodles/Testing future implementation of cleaning Spotify likes/src/main.py
@@ -6,25 +6,20 @@ from rich.traceback import install
 
 install()
 console = Console()
-curseWords = [
-    "bitch",
-    "fuck",
-    "cunt",
-    "nigga",
-    "nigger",
-    "whore",
-    "dick",
-    "pussy",
-    "shit",
-    "asshole",
-]
 
+# Declare file paths
 currentDir = path.dirname(path.abspath(__file__))
 lyricsFile = path.join(currentDir, "..", "data", "lyrics.md")
+wordsFile = path.join(currentDir, "..", "data", "forbidden_words.json")
+
+# Open the files and load their data
+with open(wordsFile, "r", encoding="utf-8") as f:
+    forbiddenWords = f.read().splitlines()
 with open(lyricsFile, "r", encoding="utf-8") as f:
     lyrics = f.read()
 
-if any(word in lyrics for word in curseWords):
+# Check the song for purity
+if any(word in lyrics for word in forbiddenWords):
     console.print("[red]The song is not clean, you should ban it[/]")
 else:
     console.print("[green]The song is clean 🙏[/]")
