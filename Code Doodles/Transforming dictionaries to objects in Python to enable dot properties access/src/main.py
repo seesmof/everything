d4be5eb14b09a16f5518c0b942cdbc9de46fe35f
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-01-11 11:47:46 +0200

    feat: well that works but no intellisense so no point

--- Code Doodles/Transforming dictionaries to objects in Python to enable dot properties access/src/main.py
+++ Code Doodles/Transforming dictionaries to objects in Python to enable dot properties access/src/main.py
@@ -1,5 +1,13 @@
-from bunch import bunchify
-
 inputData = {"a": 1, "b": {"c": 2}, "d": ["hi", {"foo": "bar"}]}
 
-x = bunchify
+from box import Box
+
+data = Box(inputData)
+
+print(data, data.a)
+
+from easydict import EasyDict
+
+data = EasyDict(inputData)
+
+print(data, data.a)
