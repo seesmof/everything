2ac63cb98a8ff99fac624ab7e01b6dd34508b095
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-01-17 20:10:47 +0200

    feat: auto mode yap
    JESUS HELP :(

--- Code Doodles/Number Guessing Game/src/main.py
+++ Code Doodles/Number Guessing Game/src/main.py
@@ -7,6 +7,7 @@ this reminded me of binary search so might wanna implement a game mode that will
 """
 
 from random import randint
+from time import sleep
 import inquirer
 from rich.console import Console
 from rich.traceback import install
@@ -16,37 +17,70 @@ console = Console()
 
 
 def getMaxNumber() -> int:
-    q = [
+    question = [
         inquirer.Text(
             "maximum number",
             message="Enter max number, up to which you want to play",
             validate=lambda _, x: x.isnumeric(),
         ),
     ]
-    a = inquirer.prompt(q)
-    return int(a["maximum number"])
+    answer = inquirer.prompt(question)
+    return int(answer["maximum number"])
 
 
 def getGuess() -> int:
-    q = [
+    question = [
         inquirer.Text(
             "guess",
             message="Enter your guess",
             validate=lambda _, x: x.isnumeric(),
         )
     ]
-    a = inquirer.prompt(q)
-    return int(a["guess"])
+    answer = inquirer.prompt(question)
+    return int(answer["guess"])
 
 
-maxNumber = getMaxNumber()
-number = randint(1, maxNumber)
-while True:
-    g = getGuess()
-    if g > number:
-        console.print("Too high")
-    elif g < number:
-        console.print("Too low")
+def getAutoMode() -> bool:
+    question = [
+        inquirer.Confirm(
+            "auto",
+            message="Enable auto mode? The game will play itself until it guesses correctly",
+        )
+    ]
+    answer = inquirer.prompt(question)
+    return answer["auto"]
+
+
+def main():
+    maxNumber = getMaxNumber()
+    number = randint(1, maxNumber)
+    autoMode = getAutoMode()
+    if not autoMode:
+        while True:
+            guess = getGuess()
+            if guess > number:
+                console.print("Too high")
+            elif guess < number:
+                console.print("Too low")
+            else:
+                console.print("Correct")
+                break
     else:
-        console.print("Correct")
-        break
+        low, high = 1, maxNumber
+
+        with console.status("Guessing..."):
+            while low <= high:
+                sleep(0.1)
+                guess = (low + high) // 2
+                if guess == number:
+                    break
+                elif guess > number:
+                    high = guess - 1
+                else:
+                    low = guess + 1
+
+        console.print(f"The number was {number}" if guess == number else "You lose")
+
+
+if __name__ == "__main__":
+    main()
