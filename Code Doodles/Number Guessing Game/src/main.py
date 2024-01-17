41f2040fde31ff370eee8058bd052e692edba29e
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-01-17 19:57:57 +0200

    feat: JESUS PLEASE HELP

---
+++ Code Doodles/Number Guessing Game/src/main.py
@@ -0,0 +1,7 @@
+"""
+i dont even know what im doing at this point. God please help me, Jesus our Almighty God
+
+so the task is to create a number guessing game - generate a random number, then prompt user to guess it and hint them if the guess is too high or too low
+
+this reminded me of binary search so might wanna implement a game mode that will play itself and show the time its done in
+"""
