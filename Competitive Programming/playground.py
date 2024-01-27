ae3ffc265ecee5bc480b354e9dce83f61891ebd6
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-01-27 15:27:36 +0200

    feat: something - LC 195
    THANKS JESUS CHRIST MY LORD AND SAVIOR 🙏

--- Competitive Programming/playground.py
+++ Competitive Programming/playground.py
@@ -10,27 +10,13 @@ console = Console()
 
 currentDir = path.dirname(path.abspath(__file__))
 dataFolder = path.join(currentDir, "data")
-phoneNumbersFile = path.join(dataFolder, "193-phone-numbers.txt")
+lineyFile = path.join(dataFolder, "195-lotta-lines.txt")
 
-numbers = []
+lines = []
 
-with open(phoneNumbersFile, "r") as file:
+with open(lineyFile, "r") as file:
     for line in file:
-        numbers.append(line.strip())
+        lines.append(line.strip())
 
-table = Table()
-table.add_column("Status", style="bold", no_wrap=True)
-table.add_column("Number", style="cyan", no_wrap=True)
-
-
-def validateNumber(number: str) -> bool:
-    pattern = "^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$|^[0-9]{3}-[0-9]{3}-[0-9]{4}$"
-    result = re.match(pattern, number)
-    return True if result else False
-
-
-for number in numbers:
-    validationResults = validateNumber(number)
-    table.add_row("[green]Good[/]" if validationResults else "[red]Bad[/]", number)
-
-console.print(table)
+tenthLine = lines[9] if len(lines) > 9 else "No more lines"
+console.print(tenthLine)
