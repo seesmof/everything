527a5231b73e25013806c650ccb82bdfd80e03f8
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-01-27 18:31:50 +0200

    feat: planning our library
    GOD HELP ME LORD 🙏

---
+++ Code Doodles/Library Inventory Manager - Continuing to play with SQL in Python/src/main.py
@@ -0,0 +1,9 @@
+"""
+LORD, please help me through this, Father. i cannot do it on my own and without your ideas, Jesus 🙏
+
+i think what would be cool to do is to have an inventory of books database, and also the orders database meaning any person can come in and order some books they would like and then those will be removed from the inventory and stored in the database
+
+we can also create a basic authentication system for our app, and for each user to have their own profile and to view the available books as well as books they have ordered
+
+we would need to create the basic Create Read Update Delete ( CRUD ) functionality for our app to work
+"""
