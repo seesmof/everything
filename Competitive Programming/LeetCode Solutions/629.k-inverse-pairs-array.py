faa76ad6f31b8930cd535318faff4deac9d7f6c4
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-01-27 15:51:33 +0200

    feat: LC 629 F
    i dont understand how it works but if its God's will, one day i will 🙏 thank you Jesus Christ my Lord and Savior, my King of Kings, my Heavenly Father

--- Competitive Programming/LeetCode Solutions/629.k-inverse-pairs-array.py
+++ Competitive Programming/LeetCode Solutions/629.k-inverse-pairs-array.py
@@ -1,3 +1,30 @@
+#
+# @lc app=leetcode id=629 lang=python3
+#
+# [629] K Inverse Pairs Array
+#
+
+# @lc code=start
+from functools import cache
+
+
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        @cache
+        def dp(n: int, k: int):
+            if k == 0:
+                return 1
+            if n == 1 or k < 0:
+                return 0
+            return dp(n, k - 1) + dp(n - 1, k) - dp(n - 1, k - n)
+
+        # return dp(n, k) % (10**9 + 7)
+        return 0
+
+
+# @lc code=end
+
+"""
 from functools import cache
 from rich.console import Console
 from rich.traceback import install
@@ -25,3 +52,4 @@ def tests():
 
 
 tests()
+"""
