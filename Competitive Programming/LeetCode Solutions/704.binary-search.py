d22e39980bf1a53cd2eac7d4307b6a25516fb9c3
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-01-29 12:05:03 +0200

    feat: LC 704
    THANK YOU JESUS 🙏 GLORY TO YOU FATHER

--- Competitive Programming/LeetCode Solutions/704.binary-search.py
+++ Competitive Programming/LeetCode Solutions/704.binary-search.py
@@ -13,14 +13,12 @@ class Solution:
         while low <= high:
             mid = (low + high) // 2
             guess = nums[mid]
-
             if guess == target:
                 return mid
             if guess > target:
                 high = mid - 1
             else:
                 low = mid + 1
-
         return -1
 
 
--- Competitive Programming/playground.py
+++ Competitive Programming/playground.py
@@ -1,3 +1,4 @@
+from typing import List
 from functools import cache
 from rich.console import Console
 from rich.traceback import install
@@ -6,21 +7,7 @@ install()
 console = Console()
 
 
-def kInversePairs(n: int, k: int) -> int:
-    @cache
-    def dp(n: int, k: int):
-        if k == 0:
-            return 1
-        if n == 1 or k < 0:
-            return 0
-        return dp(n, k - 1) + dp(n - 1, k) - dp(n - 1, k - n)
-
-    return dp(n, k) % (10**9 + 7)
-
-
 def tests():
-    assert kInversePairs(n=3, k=0) == 1
-    assert kInversePairs(n=3, k=1) == 2
     console.print("[green bold]Passed all tests![/]")
 
 
