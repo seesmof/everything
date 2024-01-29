021ceaf6fe7452ed4cf4b3cda0f000094c421c0a
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-01-29 12:29:34 +0200

    feat: rewrote binary search in playground
    thank you JESUS CHRIST OUR LORD AND SAVIOR

--- Competitive Programming/playground.py
+++ Competitive Programming/playground.py
@@ -7,7 +7,23 @@ install()
 console = Console()
 
 
+def search(nums: List[int], target: int) -> int:
+    low, high = 0, len(nums) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = nums[mid]
+        if guess == target:
+            return mid
+        if guess > target:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+
 def tests():
+    assert search(nums=[-1, 0, 3, 5, 9, 12], target=9) == 4
+    assert search(nums=[-1, 0, 3, 5, 9, 12], target=2) == -1
     console.print("[green bold]Passed all tests![/]")
 
 
