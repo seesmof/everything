3b528975cebb96fe73b8557e68b0a7453c2ab27d
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-02-09 14:39:24 +0200

    chore: im worthless
    THANKS LORD I LOVE YOU MORE THAN ANYTHING ANYWAY, THAKN YOU JESUS CHRIST MY ALMIGHTY GOD PLEASE HAVE MERCY ON ME JESUS I BEG YOU FOR FORGIVENESS

--- Competitive Programming/playground.py
+++ Competitive Programming/playground.py
@@ -6,14 +6,30 @@ from rich.traceback import install
 install()
 console = Console()
 
-
-def replace(nums: [int]) -> [int]:
-    for i, num in enumerate(nums[:-1]):
-        nums[i] = max(nums[i + 1 :])
-    nums[-1] = -1
-    return nums
+"""
+"""
 
 
-arr = [17, 18, 5, 4, 6, 1]
-res = replace(arr)
-console.print(f"{arr} -> {res}")
+def solve(s: str) -> int:
+    # form a list of all substrings
+    res = []
+    for i in range(len(s)):
+        for j in range(i + 1, len(s) + 1):
+            res.append(s[i:j])
+    console.print(res)
+
+
+def tests() -> None:
+    with console.status("[bold]Running tests...[/]"):
+        assert solve("abcabcbb") == 3, "Should be 3"
+        assert solve("bbbbb") == 1, "Should be 1"
+        assert solve("pwwkew") == 3, "Should be 3"
+        assert solve("") == 0, "Should be 0"
+        assert solve("a") == 1, "Should be 1"
+        assert solve("dvdf") == 3, "Should be 3"
+        assert solve("babad") == 3, "Should be 3"
+        assert solve("cbbd") == 2, "Should be 2"
+    console.print("[bold green]All tests passed![/]")
+
+
+solve("babad")
