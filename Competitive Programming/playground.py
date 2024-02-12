8bdb9414e29275ad6d49b2b0b33097b17ee71802
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-02-12 13:03:25 +0200

    feat: thank you Jesus
    CHRIST MY HOLY GOD THANKS A LOT LORD

--- Competitive Programming/playground.py
+++ Competitive Programming/playground.py
@@ -6,30 +6,10 @@ from rich.traceback import install
 install()
 console = Console()
 
-"""
-"""
+number: int = 3000500722
+console.print(f"{number:_}")
 
+Amen: str = "Jesus Christ is our Lord"
+console.print(f"[bold]{Amen}[/]")
 
-def solve(s: str) -> int:
-    # form a list of all substrings
-    res = []
-    for i in range(len(s)):
-        for j in range(i + 1, len(s) + 1):
-            res.append(s[i:j])
-    console.print(res)
-
-
-def tests() -> None:
-    with console.status("[bold]Running tests...[/]"):
-        assert solve("abcabcbb") == 3, "Should be 3"
-        assert solve("bbbbb") == 1, "Should be 1"
-        assert solve("pwwkew") == 3, "Should be 3"
-        assert solve("") == 0, "Should be 0"
-        assert solve("a") == 1, "Should be 1"
-        assert solve("dvdf") == 3, "Should be 3"
-        assert solve("babad") == 3, "Should be 3"
-        assert solve("cbbd") == 2, "Should be 2"
-    console.print("[bold green]All tests passed![/]")
-
-
-solve("babad")
+console.print(f"{number = }")
