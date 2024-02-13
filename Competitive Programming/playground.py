af22c4e75ce2e107901a43bfc36f8a3b3611250f
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-02-13 17:15:13 +0200

    feat: thanks God
    thanks Lord my Heavenly Father Jesus Christ please have mercy on me Lord

--- Competitive Programming/playground.py
+++ Competitive Programming/playground.py
@@ -7,23 +7,5 @@ from rich.traceback import install
 install()
 console = Console()
 
-
-def getN() -> int:
-    askForN = [
-        inquirer.Text("n", message="Enter n", validate=lambda _, x: x.isnumeric())
-    ]
-    lookForN = inquirer.prompt(askForN)
-    return int(lookForN["n"])
-
-
-allOptions = [1, 2, 5, 10, 20, 50, 100, 200, 500]
-allOptions = sorted(allOptions, reverse=True)
-n = int(input())
-takes = 0
-for meow in allOptions:
-    fit = n // meow
-    takes += fit
-    n -= fit * meow
-    if n == 0:
-        break
-print(takes)
+l, k, d, x = map(int, input().split())
+print(l + k + d + x)
