890dd209765b231f1bbf8480ee9a0ae86d1bfb24
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-02-14 11:43:50 +0200

    feat: thanks Jesus
    THANK YOU JESUS CHRIST MY LORD AND SAVIOR THANK YOU ABBA FATHER

--- Competitive Programming/playground.py
+++ Competitive Programming/playground.py
@@ -7,5 +7,17 @@ from rich.traceback import install
 install()
 console = Console()
 
-l, k, d, x = map(int, input().split())
-print(l + k + d + x)
+A, B, C = 0, 0, 0
+n = int(input())
+arr = []
+for _ in range(n):
+    a, b, c = map(int, input().split())
+    arr.append((a, b, c))
+    A += a
+    B += b
+    C += c
+A, B, C = A // n, B // n, C // n
+res = 0
+for a, b, c in arr:
+    res += abs(a - A) + abs(b - B) + abs(c - C)
+print(res)
