941a9581bda31935eb743fe82b66528bd9532728
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-02-23 12:32:06 +0200

    feat: user files KP
    Thank you so much Lord Jesus Christ our Heavenly Father our King of Kings and our Lord of Lords thank you Jesus my Holy God

--- Competitive Programming/playground.py
+++ Competitive Programming/playground.py
@@ -1,28 +1,13 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from time import sleep
+from rich.markdown import Markdown as md
+from rich.table import Table
+from rich.console import Console
+from rich.traceback import install
 
-# Define the range for x1 and x2
-x1_range = np.linspace(-10, 10, 400)
-x2_range = np.linspace(-10, 10, 400)
+install()
+console = Console()
 
-# Inequality
-x2_ineq = (10 + 3 * x1_range) / 2
-plt.plot(x1_range, x2_ineq, label="$-3x_1 +  2x_2 \leq  10$")
-
-# Equation
-x2_eq = (10 + 3 * x1_range) / 2
-plt.plot(x1_range, x2_eq, label="$-3x_1 +  2x_2 =  10$")
-
-# Derived equations
-x1_derived = -10 / 3 - 2 / 3 * x2_range
-x2_derived = 5 + 3 / 2 * x1_range
-plt.plot(x1_derived, x2_range, label="$x_1 = -\frac{10}{3} - \frac{2}{3}x_2$")
-plt.plot(x1_range, x2_derived, label="$x_2 =  5 + \frac{3}{2}x_1$")
-
-# Plot settings
-plt.xlabel("$x_1$")
-plt.ylabel("$x_2$")
-plt.title("Plot of Inequalities and Equations")
-plt.legend()
-plt.grid(True)
-plt.show()
+make = [6, 9, 8, 7, 5, 4, 9, 1, 0]
+with console.status("Working on it..."):
+    for i in make:
+        sleep(0.5)
