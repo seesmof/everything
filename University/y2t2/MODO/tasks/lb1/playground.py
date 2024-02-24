e3e0d2a987888c23923a57557e86244f1c552d7f
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-02-24 20:05:45 +0200

    feat: how great is our God sing with me how great is our GOD! the lion and the lamb, the lion and the lamb, the lion and the lamb! name above all names! worthy of all praise! how great, oh how great is our God! oh Lord Jesus thank you so much Lord my God my Heavenly Father for saving and protecting and guiding and building us up and making sure Your will, Father God, be done forever and always. Lord Jesus thank you so much Christ our Almighty King of Kings
    Thank you Jesus Christ i love you Lord Jesus

--- University/y2t2/MODO/tasks/lb1/playground.py
+++ University/y2t2/MODO/tasks/lb1/playground.py
@@ -7,9 +7,12 @@ y=3
 
 y=0
 x=3
-
-
 """
+
+
+def getSlope(X1: list[int], X2: list[int]) -> float:
+    return (X2[1] - X2[0]) / (X1[1] - X1[0])
+
 
 from matplotlib import pyplot as plt
 import numpy as np
@@ -31,24 +34,29 @@ plt.yticks(np.arange(-6, 7, 1))
 plt.axhline(color="black")
 plt.axvline(color="black")
 
-# 3X1-2X2>=-6
+# 3X1 - 2X2 > =-6
 oneX2 = [0, 3]
 oneX1 = [-2, 0]
-slope = (oneX2[1] - oneX2[0]) / (oneX1[1] - oneX1[0])
+slope = getSlope(X1=oneX1, X2=oneX2)
 x = np.linspace(-6, 6, 100)
 y = slope * (x - oneX1[0]) + oneX2[0]
 plt.plot(x, y, color="red", label="3x1 - 2x2 >= -6")
 
-# X1+X2>=3
-twoX1 = np.linspace(-6, 6, 100)
-twoX2 = 3 - twoX1
-plt.plot(twoX1, twoX2, color="green", label="x1 + x2 >= 3")
+# X1 + X2 >= 3
+twoX1 = [0, 3]
+twoX2 = [3, 0]
+slope = getSlope(X1=twoX1, X2=twoX2)
+x = np.linspace(-6, 6, 100)
+y = slope * (x - twoX1[0]) + twoX2[0]
+plt.plot(x, y, color="green", label="x1 + x2 >= 3")
 
-# X1<=3
+"""
+# X1 <= 3
 plt.axvline(x=3, color="blue", label="x1 <= 3")
 
-# X2<=5
+# X2 <= 5
 plt.axhline(y=5, color="orange", label="x2 <= 5")
+"""
 
 plt.grid()
 plt.legend()
