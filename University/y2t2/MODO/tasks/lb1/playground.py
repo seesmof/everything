2368628b0eff6c26cf1fe1717f8f6b3c7f7f3b28
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-02-24 19:00:46 +0200

    feat: thank you so much Lord Jesus
    King of Kings our Lord of Lords Jesus Christ thank you so much Father God in Jesus Holy name i pray Lord Amen

--- University/y2t2/MODO/tasks/lb1/playground.py
+++ University/y2t2/MODO/tasks/lb1/playground.py
@@ -20,15 +20,24 @@ plt.title("Багатокутник рішень")
 plt.xlabel("Вісь X₁")
 plt.ylabel("Вісь X₂")
 
+# defining the visible area of our plot
 plt.xlim(-5, 5)
 plt.ylim(-5, 5)
 
+# making grid more dense markign every 1 unit
 plt.xticks(np.arange(-5, 6, 1))
 plt.yticks(np.arange(-5, 6, 1))
 
+# marking X and Y axis with black lines
+plt.axhline(color="black")
+plt.axvline(color="black")
+
 oneX2 = [0, 3]
 oneX1 = [-2, 0]
-plt.plot(oneX1, oneX2, color="red", label="3x1 - 2x2 = -6")
+slope = (oneX2[1] - oneX2[0]) / (oneX1[1] - oneX1[0])
+x = np.linspace(-5, 5, 100)
+y = slope * (x - oneX1[0]) + oneX2[0]
+plt.plot(x, y, color="red", label="3x1 - 2x2 = -6")
 
 plt.grid()
 plt.legend()
