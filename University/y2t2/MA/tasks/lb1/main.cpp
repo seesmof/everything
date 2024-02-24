b575ee76df62335d87ba55c6149585ccb0aec371
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-02-24 11:12:05 +0200

    feat: okay that dont work
    Thank you Lord Jesus if you wanna please help me in it in the name of Jesus Christ our Lord and Savior Father God let not my will, but Your will be done, Lord Jesus. In Jesus Holy name Amen

--- University/y2t2/MA/tasks/lb1/main.cpp
+++ University/y2t2/MA/tasks/lb1/main.cpp
@@ -1,8 +1,40 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include "../Simc23_VS/simc.h"
 
-int main() {
-  cout << "Hello, World!" << endl;
+int main()
+{
+  // initializing our main facility - a chocolate factory
+  pfacility mainFascility;
+  // allocating memory for it
+  initlist(100);
+  // creating a first chocolate batch right as our factory opens
+  initcreate(1, 0);
+  // creating our chocolate factory
+  newfac(mainFascility, "Chocolate Factory");
+  // run our factory for 100 hours
+  while (systime < 100)
+  {
+    // start the chocolate production
+    plan();
+    switch (sysevent)
+    {
+    case 1:
+      create(10);
+      break;
+    case 2:
+      seize(mainFascility);
+      break;
+    case 3:
+      delayt(20);
+      break;
+    case 4:
+      outfac(mainFascility);
+      break;
+    case 5:
+      destroy();
+      break;
+    }
+  }
+  // view the results of our production
+  printall();
   return 0;
 }
