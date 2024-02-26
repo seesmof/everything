ccfca75d2c9b13cc885d531109c1a17333fd2518
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-02-26 09:05:05 +0200

    feat: thank you Jesus King our Lord and Savior. im exhausted but please heal me Father God if its your will Lord Jesus please forgive me if i did anything wrong Lord God i repent please have mercy on me Lord Jesus Christ our Savior and our King of Kings and our Lord of Lords
    thank you Lord Jesus please have mercy on me Father God my Lord and Savior

--- Competitive Programming/playground.py
+++ Competitive Programming/playground.py
@@ -1,4 +1,3 @@
-from time import sleep
 from rich.markdown import Markdown as md
 from rich.table import Table
 from rich.console import Console
@@ -7,7 +6,32 @@ from rich.traceback import install
 install()
 console = Console()
 
-make = [6, 9, 8, 7, 5, 4, 9, 1, 0]
-with console.status("Working on it..."):
-    for i in make:
-        sleep(0.5)
+from functools import wraps
+from time import perf_counter
+
+
+def memoize(fn):
+    cache = {}
+
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key not in cache:
+            cache[key] = fn(*args, **kwargs)
+        return cache[key]
+
+    return wrapper
+
+
+@memoize
+def fib(n: int) -> int:
+    if n <= 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+
+startTimer = perf_counter()
+with console.status("[bold green]Calculating..."):
+    res = fib(100)
+timeTaken = perf_counter() - startTimer
+console.print(f"Calculated {res = } in {timeTaken:.1f} seconds")
