0c87db493e8a3aab94cddfbcca1f51014f0c4804
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-02-27 22:58:11 +0200

    feat: thank you so much Lord Jesus Christ our Heavenly Father our Perfect our Righteous our Holy Lord God the Father and God the Son Lord Jesus Christ and the Holy Spirit Lord Jesus Christ i thank you so much Lord God for helping and directing and protecting me in Jesus Holy name i pray Lord God Amen and Amen
    GLORY TO GOD OUR LORD AND SAVIOR CHRIST JESUS OUR LORD AND OUR KING OF KINGS AMEN AND AMEN HALLEULUJAH

--- Python Automations/playground.py
+++ Python Automations/playground.py
@@ -1,7 +1,16 @@
-from src.utils.misc import getClothing
+from os import path
+from src.utils.misc import getClothing, readJson
+from rich.markdown import Markdown as md
+from rich.table import Table
+from rich.console import Console
+from rich.traceback import install
+
+install()
+console = Console()
 
 
-weathers = [-3, 4, 6, 9, 12, 16, 30, "-3°C"]
-for weather in weathers:
-    clothes = getClothing(weather)
-    print(f"{weather}: {clothes}")
+currentDir = path.dirname(path.abspath(__file__))
+versesPath = path.join(currentDir, "data", "Bible_verses.json")
+verses = readJson(versesPath)
+
+console.print(verses, len(verses))
