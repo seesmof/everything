691358cff877fbd9c144ada4335a522572b82055
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-03-16 12:36:08 +0200

    feat: renaming Psalm songs mp3 file names, thanks Jesus Christ my Holy Almighty GOD King Jesus Christ
    God bless Ukraine Lord GOD please protect us O Lord GOD Almighty King Jesus Christ

--- Python Automations/playground.py
+++ Python Automations/playground.py
@@ -1,3 +1,5 @@
+from os import path
+import os
 from rich.markdown import Markdown as md
 from rich.table import Table
 from rich.console import Console
@@ -6,5 +8,47 @@ from rich.traceback import install
 install()
 console = Console()
 
-arr = [123, 321, 111]
-console.print(arr, 3 in arr)
+patternsToRemove: list[str] = ["🎤 ", "yt1s.com -  "]
+targetDir: str = "C:\\Users\\seesm\\Downloads\\"
+replacePattern: dict = {"  ": " - "}
+
+
+def removePatterns(dir: str, patterns: list[str]) -> None:
+    files = [f for f in os.listdir(dir)]
+
+    for file in files:
+        newName = file
+        for pattern in patterns:
+            if newName.startswith(pattern):
+                newName = newName[len(pattern) :]
+        targetFile = path.join(dir, newName)
+        if newName != file:
+            try:
+                os.rename(path.join(dir, file), targetFile)
+                console.print(f"Renamed {file} to {newName}")
+            except FileExistsError:
+                console.print(f"Error: {newName} already exists.")
+            except Exception as e:
+                console.print(f"Error renaming {file}: {e}")
+        else:
+            console.print(f"No change for {file}")
+
+
+def replaceSpaces(dir: str, replace: dict) -> None:
+    files = [f for f in os.listdir(dir)]
+
+    for file in files:
+        newName = file
+        for pattern, replacement in replace.items():
+            newName = newName.replace(pattern, replacement)
+        targetFile = path.join(dir, newName)
+        if newName != file:
+            try:
+                os.rename(path.join(dir, file), targetFile)
+                console.print(f"Renamed {file} to {newName}")
+            except FileExistsError:
+                console.print(f"Error: {newName} already exists.")
+            except Exception as e:
+                console.print(f"Error renaming {file}: {e}")
+        else:
+            console.print(f"No change for {file}")
