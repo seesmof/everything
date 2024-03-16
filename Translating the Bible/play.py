64278b778537246823cda6a020644fc2aa927b98
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-03-16 15:05:57 +0200

    feat: trying to automate this somehow. thinking of creating an md document for each chapter instead. thanks Lord GOD Almighty King Jesus Christ my Lord of Lords and King of Kings Hallelujah
    thank You Jesus Christ my Holy GOD Almighty King Jesus Christ my Lord

--- Translating the Bible/play.py
+++ Translating the Bible/play.py
@@ -1,76 +1,4 @@
-Bible_Books = {
-    "Old Testament": [
-        "Genesis",
-        "Exodus",
-        "Leviticus",
-        "Numbers",
-        "Deuteronomy",
-        "Joshua",
-        "Judges",
-        "Ruth",
-        "1 Samuel",
-        "2 Samuel",
-        "1 Kings",
-        "2 Kings",
-        "1 Chronicles",
-        "2 Chronicles",
-        "Ezra",
-        "Nehemiah",
-        "Esther",
-        "Job",
-        "Psalms",
-        "Proverbs",
-        "Ecclesiastes",
-        "Song of Solomon",
-        "Isaiah",
-        "Jeremiah",
-        "Lamentations",
-        "Ezekiel",
-        "Daniel",
-        "Hosea",
-        "Joel",
-        "Amos",
-        "Obadiah",
-        "Jonah",
-        "Micah",
-        "Nahum",
-        "Habakkuk",
-        "Zephaniah",
-        "Haggai",
-        "Zechariah",
-        "Malachi",
-    ],
-    "New Testament": [
-        "Matthew",
-        "Mark",
-        "Luke",
-        "John",
-        "Acts",
-        "Romans",
-        "1 Corinthians",
-        "2 Corinthians",
-        "Galatians",
-        "Ephesians",
-        "Philippians",
-        "Colossians",
-        "1 Thessalonians",
-        "2 Thessalonians",
-        "1 Timothy",
-        "2 Timothy",
-        "Titus",
-        "Philemon",
-        "Hebrews",
-        "James",
-        "1 Peter",
-        "2 Peter",
-        "1 John",
-        "2 John",
-        "3 John",
-        "Jude",
-        "Revelation",
-    ],
-}
-
+import json
 from os import path
 import os
 from rich.markdown import Markdown as md
@@ -81,16 +9,15 @@ from rich.traceback import install
 install()
 console = Console()
 
-console.print(
-    f"Books in the Old Testament: {len(Bible_Books['Old Testament'])} and Books in the New Testament: {len(Bible_Books['New Testament'])}.\n{len(Bible_Books['Old Testament']) + len(Bible_Books['New Testament'])} total Books."
-)
 currentDir: str = path.dirname(path.abspath(__file__))
-currentTranslation: str = path.join(currentDir, "AMP to UKR")
-OT_Folder = path.join(currentTranslation, "Old Testament")
-NT_Folder = path.join(currentTranslation, "New Testament")
-
-# create folder for each book in both New and Old Testament folders
-for book in Bible_Books["Old Testament"]:
-    os.makedirs(path.join(OT_Folder, book), exist_ok=True)
-for book in Bible_Books["New Testament"]:
-    os.makedirs(path.join(NT_Folder, book), exist_ok=True)
+mainFilePath: str = path.join(currentDir, "AMP to UKR", "main.json")
+main = {}
+with open(mainFilePath, "r", encoding="utf-8") as jsonFile:
+    main = json.load(jsonFile)
+for Testament in main:
+    for Book in main[Testament]:
+        ChaptersCount = Book["Chapters_Count"]
+        Chapters = {f"{i+1}": {"1": ""} for i in range(ChaptersCount)}
+        Book["Chapters"] = Chapters
+with open(mainFilePath, "w", encoding="utf-8") as jsonFile:
+    json.dump(main, jsonFile, indent=2)
