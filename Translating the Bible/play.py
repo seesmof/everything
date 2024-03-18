9a2beff3c1a9cd6e56a9356e6833b03801a9d1ad
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-03-18 09:32:53 +0200

    feat: trying AMP to UKR. thanks Lord Jesus Christ my Lord and my Savior Almighty GOD Most High
    thank You Lord Jesus Christ my Lord and Savior LORD GOD

--- Translating the Bible/play.py
+++ Translating the Bible/play.py
@@ -10,15 +10,16 @@ install()
 console = Console()
 
 currentDir: str = path.dirname(path.abspath(__file__))
-targetFolderPath: str = path.join(currentDir, "AMP to UKR")
+targetFolderPath: str = path.join(currentDir, "Сучасний Американо-Український - САУ")
 mainFilePath: str = path.join(targetFolderPath, "main.json")
+target = path.join(currentDir, "Розширений Американсько-Український - РАУ")
 
 with open(mainFilePath, "r", encoding="utf-8") as jsonFile:
     main = json.load(jsonFile)
 
 for Testament in main:
     # create folder in targetFolder for each Testament
-    targetTestamentFolder: str = path.join(targetFolderPath, Testament)
+    targetTestamentFolder: str = path.join(target, Testament)
     if not os.path.exists(targetTestamentFolder):
         os.makedirs(targetTestamentFolder)
 
@@ -30,7 +31,7 @@ for Testament in main:
             os.makedirs(targetTestamentBookFolder)
 
         for Chapter in Book["Chapters"]:
-            Chapter_File = f"{Chapter}.md"
+            Chapter_File = f"{Chapter}.docx"
             # create file in targetFolder for each Chapter
             targetTestamentBookChapterFile: str = path.join(
                 targetTestamentBookFolder, Chapter_File
@@ -39,4 +40,5 @@ for Testament in main:
                 with open(
                     targetTestamentBookChapterFile, "w", encoding="utf-8"
                 ) as mdFile:
-                    mdFile.write(f"# {Chapter}\n\n")
+                    # just create an empty file
+                    mdFile.write("")
