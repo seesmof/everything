8a97db0edfbafd08c46e4ae4580703a9320ed9e8
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-03-26 16:51:56 +0200

    feat: thank You Father GOD so much Lord Jesus Christ my Holy GOD Most High
    thank You so much Father GOD Most High my Lord Almighty King Jesus Christ my Lord and Savior

--- University/y2t2/DB/tasks/lb2/dev/supervisors.sql
+++ University/y2t2/DB/tasks/lb2/dev/supervisors.sql
@@ -1,4 +1,4 @@
-SELECT Employee.Employee_Name, Employee.Employee_Position, Position.Department_Name
+SELECT Employee.Employee_Name, Employee.Employee_Position, Position.Position_ID
 FROM [Position] 
 INNER JOIN Employee ON Position.[Position_ID] = Employee.[Employee_Position]
-WHERE Position.Department_Name = 1
+WHERE Position.[Position_ID] = 2 OR Position.[Position_ID] = 4
