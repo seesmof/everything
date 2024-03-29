bccab76ea49294c11ce9bdaededb69676168da8e
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-03-29 13:00:34 +0200

    feat: Apostles thank You Jesus Christ my Lord Most High King Jesus Christ my Lord Almgithy GOD Most High King Jesus Christ my Lord Almighty GOD Most High King Jesus Christ my Lord Almighty GOD King Jesus Christ our King of Kings and our Lord of Lords
    thank You Lord Jesus Christ my Lord and Savior our Heavely Father GOD Most High King Jesus Christ Almighty GOD

--- University/y2t2/MODO/playground.py
+++ University/y2t2/MODO/playground.py
@@ -1,13 +1,4 @@
-def f(x):
-    return (x - 2) ** 2
+"""
+38 in total
 
-
-from rich.console import Console
-from rich.traceback import install
-from scipy import optimize
-
-install()
-console = Console()
-
-res = optimize.minimize_scalar(f, bounds=(-1, 3), method="golden")
-console.print(f"Відповідь: {res.x}")
+"""
