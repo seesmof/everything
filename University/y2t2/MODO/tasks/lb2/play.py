2a9715850da54387619aad5e9d27752100cdb239
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-04-02 19:34:29 +0300

    feat: Laws of Christ Jesus our Lord Almighty GOD King Jesus Christ our Lord and Savior
    Lord GOD i really hope and pray my faith is not dead. please correct me and rebuke me O Lord my Holy GOD Christ Jesus our Lord Almighty GOD Most High please forgive me all my sins, please wash me clean with the Holy Blood of Jesus Christ our Lord Almighty GOD

--- University/y2t2/MODO/tasks/lb2/play.py
+++ University/y2t2/MODO/tasks/lb2/play.py
@@ -9,27 +9,23 @@ install()
 console = Console()
 
 
-def f(x):
+def twenty(x):
     return (x - 2) ** 2
 
 
-def f(x):
+def nineteen(x):
     return -np.log(x**2 + 1) / np.e**x
 
 
-x = np.linspace(-1, 3, 400)
-y = f(x)
-
-plt.figure(figsize=(10, 6))
-plt.plot(x, y, label="(x-2)^2")
-plt.title("Plot of (x-2)^2")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.legend()
-plt.grid(True)
+def twentyOne(x):
+    return np.e ** (x**2) - 2
 
 
-def goldenSearch(f: callable = f, a: float = -1, b: float = 3, tol: float = 1e-5):
+def twentyTwo(x):
+    return x**4 + np.cos(x) - np.log(x) ** 2
+
+
+def goldenSearch(f, a: float = -1, b: float = 3, tol: float = 1e-5):
     ratio = (5**0.5 - 1) / 2
     c = b - ratio * (b - a)
     d = a + ratio * (b - a)
@@ -59,7 +55,7 @@ def goldenSearch(f: callable = f, a: float = -1, b: float = 3, tol: float = 1e-5
     return (a + b) / 2
 
 
-def bisectionSearch(f: callable = f, a: float = -1, b: float = 3, tol: float = 1e-5):
+def bisectionSearch(f, a: float = -1, b: float = 3, tol: float = 1e-5):
     x = np.linspace(a, b, 1000)
     plt.figure(figsize=(10, 6))
     plt.plot(x, f(x), label="f(x)")
@@ -69,36 +65,31 @@ def bisectionSearch(f: callable = f, a: float = -1, b: float = 3, tol: float = 1
     plt.legend()
     plt.plot([a, b], [f(a), f(b)], "r--", label="Initial Interval")
 
-    while (b - a) >= tol:
-        c = (a + b) / 2
-        if f(c) == 0.0:
-            break
-        if f(a) * f(c) < 0:
-            b = c
+    while abs(b - a) >= tol:
+        x1 = a + (b - a) / 3
+        x2 = b - (b - a) / 3
+
+        if f(x1) < f(x2):
+            b = x2
         else:
-            a = c
+            a = x1
 
         plt.plot([a, b], [f(a), f(b)], "g--", label="Current Interval")
         plt.pause(0.1)
 
     plt.plot([a, b], [f(a), f(b)], "b--", label="Final Interval")
     plt.show()
-    return c
+    return (a + b) / 2
 
 
-with console.status("Optimizing...", spinner="point"):
-    resGolden: float = f"{goldenSearch(a=0):.2f}"
-    resBisection: float = f"{bisectionSearch(a=0):.2f}"
-    scalarBounded: float = f"{optimize.minimize_scalar(f, bounds=(0, 3)).x:.2f}"
+data = [(twenty, -1, 3), (nineteen, 0, 3), (twentyOne, -1, 1), (twentyTwo, 0.5, 1.5)]
 
-console.print(f"Golden Section Method: {resGolden}")
-console.print(f"Bisection Method: {resBisection}")
-console.print(f"Bounded Scalar (from scipy): {scalarBounded}")
-console.print()
-console.print(
-    "[green bold]Correct answer found![/green bold]"
-    if resGolden == resBisection == scalarBounded
-    else "[red bold]Doesn't match![/red bold]"
-)
+for f, a, b in data:
+    resGolden: float = f"{goldenSearch(f, a, b):.2f}"
+    resBisection: float = f"{bisectionSearch(f, a, b):.2f}"
+    scalarBounded: float = f"{optimize.minimize_scalar(f, bounds=(a, b)).x:.2f}"
 
-plt.show()
+    console.print(f"Golden Section Method: {resGolden}")
+    console.print(f"Bisection Method: {resBisection}")
+    console.print(f"Bounded Scalar (from scipy): {scalarBounded}")
+    console.print()
